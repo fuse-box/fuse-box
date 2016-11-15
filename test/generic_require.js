66f8caed162cd37e2a3ce1e5fe50ec02e878b528
Ivan.Orlov <ivan@morrr.com>
2016-11-15 22:42:15 +0200

    More tests

--- test/generic_require.js
+++ test/generic_require.js
@@ -1,9 +1,8 @@
 const should = require("should");
 const getTestEnv = require("./fixtures/lib.js").getTestEnv;
 
-
-
 describe("Generic requires", (done) => {
+
     it("Case 1", (done) => {
         getTestEnv({
             "index.js": `require("./foo/bar.js");`,
@@ -99,4 +98,97 @@ describe("Generic requires", (done) => {
         }).catch(done);
     });
 
+    it("Should require a folder", (done) => {
+        getTestEnv({
+            "index.js": `exports.foo = require("./foo")`,
+            "foo/index.js": `module.exports = {bar : 10}`,
+        }, "**/*.js").then(root => {
+            let result = root.FuseBox.import("index");
+            result.should.deepEqual({ foo: { bar: 10 } });
+            done();
+        }).catch(done);
+    });
+
+    it("Should require a folder with tilde", (done) => {
+        getTestEnv({
+            "index.js": `exports.foo = require("./foo")`,
+            "foo/index.js": `module.exports = require("~/bar")`,
+            "bar/index.js": `module.exports = {bar : 20}`,
+        }, "**/*.js").then(root => {
+            let result = root.FuseBox.import("index");
+            result.should.deepEqual({ foo: { bar: 20 } });
+            done();
+        }).catch(done);
+    });
+
+    it("Should require a folder with tilde (without ext)", (done) => {
+        getTestEnv({
+            "index.js": `exports.foo = require("./foo")`,
+            "foo/index.js": `module.exports = require("~/bar/index")`,
+            "bar/index.js": `module.exports = {bar : 20}`,
+        }, "**/*.js").then(root => {
+            let result = root.FuseBox.import("index");
+            result.should.deepEqual({ foo: { bar: 20 } });
+            done();
+        }).catch(done);
+    });
+
+    it("Should require a folder with tilde (with ext)", (done) => {
+        getTestEnv({
+            "index.js": `exports.foo = require("./foo")`,
+            "foo/index.js": `module.exports = require("~/bar/index.js")`,
+            "bar/index.js": `module.exports = {bar : 20}`,
+        }, "**/*.js").then(root => {
+            let result = root.FuseBox.import("index");
+            result.should.deepEqual({ foo: { bar: 20 } });
+            done();
+        }).catch(done);
+    });
+
+    it("Should require a relative require statement (folder)", (done) => {
+        getTestEnv({
+            "index.js": `exports.foo = require("./foo")`,
+            "foo/index.js": `module.exports = require("../bar")`,
+            "bar/index.js": `module.exports = {bar : 30}`,
+        }, "**/*.js").then(root => {
+            let result = root.FuseBox.import("index");
+            result.should.deepEqual({ foo: { bar: 30 } });
+            done();
+        }).catch(done);
+    });
+
+    it("Should require a relative require statement (file without ext)", (done) => {
+        getTestEnv({
+            "index.js": `exports.foo = require("./foo")`,
+            "foo/index.js": `module.exports = require("../bar/index")`,
+            "bar/index.js": `module.exports = {bar : 30}`,
+        }, "**/*.js").then(root => {
+            let result = root.FuseBox.import("index");
+            result.should.deepEqual({ foo: { bar: 30 } });
+            done();
+        }).catch(done);
+    });
+
+    it("Should require a relative require statement (file with ext)", (done) => {
+        getTestEnv({
+            "index.js": `exports.foo = require("./foo")`,
+            "foo/index.js": `module.exports = require("../bar/index.js")`,
+            "bar/index.js": `module.exports = {bar : 30}`,
+        }, "**/*.js").then(root => {
+            let result = root.FuseBox.import("index");
+            result.should.deepEqual({ foo: { bar: 30 } });
+            done();
+        }).catch(done);
+    });
+
+    it("Should handle circular dependency", (done) => {
+        getTestEnv({
+            "a.js": `exports.a = 1; require("./b")`,
+            "b.js": `exports.b = require("./a")`,
+        }, "**/*.js").then(root => {
+            let result = root.FuseBox.import("b");
+            result.should.deepEqual({ b: { a: 1 } });
+            done();
+        }).catch(done);
+    });
 })
