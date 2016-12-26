0533afa426789b7188c43bb447a6cb33c2c5ae85
Ivan.Orlov <ivan@morrr.com>
2016-12-26 15:18:18 +0200

    Release 1.3.26

---
+++ src/ASTTraverse.ts
@@ -0,0 +1,45 @@
+export class ASTTraverse {
+    public static traverse(root, options) {
+
+        options = options || {};
+        let pre = options.pre;
+        let post = options.post;
+        let skipProperty = options.skipProperty;
+
+        let visit = (node, parent, prop?, idx?) => {
+            if (!node || typeof node.type !== "string") {
+                return;
+            }
+            if (node._visited) {
+                return;
+            }
+            let res = undefined;
+            if (pre) {
+                res = pre(node, parent, prop, idx);
+            }
+
+            node._visited = true;
+
+            if (res !== false) {
+                for (let prop in node) {
+                    if (skipProperty ? skipProperty(prop, node) : prop[0] === "$") {
+                        continue;
+                    }
+                    let child = node[prop];
+                    if (Array.isArray(child)) {
+                        for (let i = 0; i < child.length; i++) {
+                            visit(child[i], node, prop, i);
+                        }
+                    } else {
+                        visit(child, node, prop);
+                    }
+                }
+            }
+
+            if (post) {
+                post(node, parent, prop, idx);
+            }
+        };
+        visit(root, null);
+    }
+}
