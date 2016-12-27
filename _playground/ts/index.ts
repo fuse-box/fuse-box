ac0d0bc533c564bef6c6357703e6516a74ed2dbc
Ivan.Orlov <ivan@morrr.com>
2016-12-27 23:05:17 +0200

    CSS sourcemaps and writing

--- _playground/ts/index.ts
+++ _playground/ts/index.ts
@@ -1,5 +1,3 @@
 import "./styles.css"
+import "./test.scss";
 import "./less/styles.less";
-
-const rawStyles = require("./less/styles.raw.less?raw")
-document.querySelector('.style-example').innerHTML += rawStyles.default;
