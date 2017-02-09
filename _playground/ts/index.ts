74fae0757d7be45fcca841bc5af1618580ab91db
Ivan.Orlov <ivan@morrr.com>
2017-02-09 10:12:23 +0100

    Object assign polyfill

--- _playground/ts/index.ts
+++ _playground/ts/index.ts
@@ -2,7 +2,7 @@ import "jstree/dist/jstree.js";
 import "jstree/dist/themes/default/style.css";
 import "./styles.css";
 
-
+console.log(process.env);
 
 $(function () {
     $('#container').jstree();
