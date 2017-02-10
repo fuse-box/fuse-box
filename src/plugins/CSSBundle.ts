15a074ee20c8214128da6879dc6950afa01a68eb
Ivan.Orlov <ivan@morrr.com>
2017-02-10 12:44:00 +0100

    Prepairing CSSbundle

--- src/plugins/CSSBundle.ts
+++ src/plugins/CSSBundle.ts
@@ -10,37 +10,12 @@ import { Plugin } from "../WorkflowContext";
  * @implements {Plugin}
  */
 export class CSSBundleClass implements Plugin {
-
-    constructor(opts: any = {}) {
-        // if ("ext" in opts) { this.ext = opts.ext; }
-        // if ("name" in opts) { this.bundleName = opts.name; }
-        // if ("delimiter" in opts) { this.delimiter = opts.delimiter; }
-    }
-    /**
-     * 
-     * 
-     * @type {RegExp}
-     * @memberOf FuseBoxHTMLPlugin
-     */
     public test: RegExp = /\.css$/;
-
-    /**
-     * 
-     * 
-     * @param {WorkFlowContext} context
-     * 
-     * @memberOf FuseBoxHTMLPlugin
-     */
-    public init(context: WorkFlowContext) {
-
-    }
+    constructor(opts: any = {}) { }
+    public init(context: WorkFlowContext) { }
 
     public transformGroup(group: File) {
         let contents = [];
-        console.log("tranform group", group);
-        // group.subFiles.forEach(file => {
-        //     contents.push(file.contents);
-        // });
     }
 
 };
