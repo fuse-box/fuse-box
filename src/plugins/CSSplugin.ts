13529a8b72b2eb1d73375280a25f605830a94939
Ivan.Orlov <ivan@morrr.com>
2017-02-18 09:26:51 +0100

    CSSPlugin write options deprecated :fire:

--- src/plugins/CSSplugin.ts
+++ src/plugins/CSSplugin.ts
@@ -1,4 +1,4 @@
-import { ensureUserPath, replaceExt } from '../Utils';
+
 import * as fs from "fs";
 import * as path from "path";
 import { Config } from "./../Config";
@@ -6,6 +6,7 @@ import { File } from "../File";
 import { WorkFlowContext } from "./../WorkflowContext";
 import { Plugin } from "../WorkflowContext";
 import { utils } from "realm-utils";
+import { CSSPluginDeprecated } from './CSSPluginDeprecated';
 /**
  *
  *
@@ -98,54 +99,9 @@ export class CSSPluginClass implements Plugin {
             return;
         }
         if (this.writeOptions) {
-            if (!utils.isPlainObject(this.writeOptions)) {
-                this.writeOptions = {};
-            }
-            // can't work without outFile
-            if (file.context.outFile) {
-                // Get a directory of our bundle
-                let base = path.dirname(file.context.outFile);
-                // Change project path extension to .css
-                let projectPath = replaceExt(file.info.fuseBoxPath, ".css");
-
-                // Get new path based on where outFile is located + real project path
-                // Making sure here that folders are created
-                let newPath = ensureUserPath(path.join(base, projectPath));
-
-                let initialContents = file.contents;
-                file.contents = `__fsbx_css("${projectPath}")`;
-
-                let tasks = [];
-                if (file.sourceMap) {
-                    let sourceMapFile = projectPath + ".map";
-                    // adding sourcemap link to a file
-                    // Sometimes it's already there (written by SASS for example)
-                    // What shall we do then...
-                    initialContents += `\n/*# sourceMappingURL=${sourceMapFile} */`;
-
-                    let souceMapPath = ensureUserPath(path.join(base, sourceMapFile));
-                    let initialSourceMap = file.sourceMap;
-                    file.sourceMap = undefined;
-                    tasks.push(new Promise((resolve, reject) => {
-                        fs.writeFile(souceMapPath, initialSourceMap, (err, res) => {
-                            if (err) {
-                                return reject(err);
-                            }
-                            return resolve();
-                        });
-                    }));
-                }
-
-                // writing a file
-                tasks.push(new Promise((resolve, reject) => {
-                    fs.writeFile(newPath, initialContents, (err, res) => {
-                        if (err) {
-                            return reject(err);
-                        }
-                        return resolve();
-                    });
-                }));
-                return Promise.all(tasks);
+            const writeResult = CSSPluginDeprecated.writeOptions(this.writeOptions, file);
+            if (writeResult) {
+                return writeResult;
             }
         } else {
             file.sourceMap = undefined;
