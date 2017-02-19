1500acdaf15a1405b99a1a434b73dae4e6a60b03
Ivan.Orlov <ivan@morrr.com>
2017-02-19 23:37:41 +0100

    CSSTests

---
+++ test/css_plugin.js
@@ -0,0 +1,98 @@
+const should = require('should');
+const { CSSPlugin } = require(`../dist/commonjs/index.js`);
+const path = require("path");
+const { getTestEnv, createEnv } = require("./fixtures/lib.js")
+const fs = require("fs");
+const mkdirp = require("mkdirp");
+
+const appRoot = require("app-root-path");
+
+
+let tmp, shouldExistж
+
+const makeTestFolder = () => {
+    tmp = path.join(appRoot.path, ".fusebox", "css-test", new Date().getTime().toString());
+    mkdirp(tmp);
+    shouldExist = (name) => {
+        const fname = path.join(tmp, name);;
+        should.equal(fs.existsSync(fname), true);
+    }
+}
+
+
+describe('CSSPlugins ', () => {
+
+
+    it("Should require and inline a simple CSS File", (done) => {
+        makeTestFolder();
+
+        createEnv({
+            project: {
+                files: {
+                    "index.ts": `exports.hello = { bar : require("./main.css") }`,
+                    "main.css": "body {}"
+                },
+                plugins: [CSSPlugin()],
+                instructions: "> index.ts"
+            }
+        }).then((result) => {
+            const js = result.projectContents.toString();
+            should.equal(
+                js.indexOf(`__fsbx_css("main.css", "body {}")`) > -1, true);
+            done();
+        }).catch(done)
+    });
+
+    it("Should require and create a simple CSS File", (done) => {
+        makeTestFolder();
+
+        createEnv({
+            project: {
+                files: {
+                    "index.ts": `exports.hello = { bar : require("./main.css") }`,
+                    "main.css": "body {}"
+                },
+                plugins: [
+                    CSSPlugin({
+                        outFile: (file) => `${tmp}/${file}`
+                    })
+                ],
+                instructions: "> index.ts"
+            }
+        }).then((result) => {
+            const js = result.projectContents.toString();
+            shouldExist("main.css");
+            should.equal(
+                js.indexOf(`__fsbx_css("main.css");`) > -1, true);
+            done();
+        }).catch(done)
+    });
+
+
+    it("Should create a CSS File but not inject it", (done) => {
+        makeTestFolder();
+        createEnv({
+            project: {
+                files: {
+                    "index.ts": `exports.hello = { bar : require("./main.css") }`,
+                    "main.css": "h1 {}"
+                },
+                plugins: [
+                    CSSPlugin({
+                        outFile: (file) => `${tmp}/${file}`,
+                        inject: false
+                    })
+                ],
+                instructions: "> index.ts"
+            }
+        }).then((result) => {
+            const js = result.projectContents.toString();
+            shouldExist("main.css");
+            should.equal(
+                js.indexOf(`__fsbx_css("main.css");`) === -1, true);
+            done();
+        }).catch(done)
+    });
+
+
+});
