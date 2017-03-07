272e9a5c12c842ffdee24f5426f6fe0076a24662
Arete Code <aretecode@gmail.com>
2017-03-07 09:03:28 -0800

    make travis use gulp devDeps to simplify managing of devDeps, lint gulp file, simplify gulp imports

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,16 @@
-const gulp = require('gulp');
-const rename = require('gulp-rename');
-const clean = require('gulp-clean');
-const replace = require('gulp-replace');
-const ts = require('gulp-typescript');
-const concat = require('gulp-concat');
-const fs = require('fs');
-const sourcemaps = require('gulp-sourcemaps');
-const runSequence = require('run-sequence');
-const bump = require('gulp-bump');
-const child_process = require('child_process');
-const spawn = child_process.spawn;
-const wrap = require('gulp-wrap');
-const uglify = require('gulp-uglify');
-const changelog = require('gulp-changelog-generator');
+const gulp = require("gulp");
+const rename = require("gulp-rename");
+const clean = require("gulp-clean");
+const replace = require("gulp-replace");
+const ts = require("gulp-typescript");
+const sourcemaps = require("gulp-sourcemaps");
+const runSequence = require("run-sequence");
+const bump = require("gulp-bump");
+const wrap = require("gulp-wrap");
+const uglify = require("gulp-uglify");
+const changelog = require("gulp-changelog-generator");
+const { spawn } = require("child_process");
+const fs = require("fs");
 
 /**
  * Fail on error if not in watch mode
@@ -28,116 +26,116 @@ function onError(error) {
 /**
  * ts projects
  */
-let projectTypings = ts.createProject('src/tsconfig.json', {
+let projectTypings = ts.createProject("src/tsconfig.json", {
     removeComments: false,
 });
-let projectCommonjs = ts.createProject('src/tsconfig.json');
-let projectLoader = ts.createProject('src/loader/tsconfig.json');
-let projectLoaderTypings = ts.createProject('src/loader/tsconfig.json', {
+let projectCommonjs = ts.createProject("src/tsconfig.json");
+let projectLoader = ts.createProject("src/loader/tsconfig.json");
+let projectLoaderTypings = ts.createProject("src/loader/tsconfig.json", {
     removeComments: false,
 });
-let projectModule = ts.createProject('src/modules/tsconfig.json');
+let projectModule = ts.createProject("src/modules/tsconfig.json");
 
 /**
  * Our commonjs only files
  */
-let filesMain = ['src/**/*.ts', '!./**/*test*.ts', '!./src/loader/LoaderAPI.ts', '!./src/modules/**/*.ts'];
+let filesMain = ["src/**/*.ts", "!./**/*test*.ts", "!./src/loader/LoaderAPI.ts", "!./src/modules/**/*.ts"];
 
 /**
  * Loader API building
  */
-gulp.task('dist-loader-js', () => {
-    return gulp.src('src/loader/LoaderAPI.ts')
-        .pipe(projectLoader()).on('error', onError).js
+gulp.task("dist-loader-js", () => {
+    return gulp.src("src/loader/LoaderAPI.ts")
+        .pipe(projectLoader()).on("error", onError).js
         .pipe(wrap(`(function(__root__){
 if (__root__["FuseBox"]) return __root__["FuseBox"];
 <%= contents %>
 return __root__["FuseBox"] = FuseBox; } )(this)`))
-        .pipe(rename('fusebox.js'))
-        .pipe(gulp.dest('modules/fuse-box-loader-api'))
-        .pipe(rename('fusebox.min.js'))
+        .pipe(rename("fusebox.js"))
+        .pipe(gulp.dest("modules/fuse-box-loader-api"))
+        .pipe(rename("fusebox.min.js"))
         .pipe(uglify())
-        .pipe(replace(/;$/, ''))
-        .pipe(replace(/^\!/, ''))
-        .pipe(gulp.dest('modules/fuse-box-loader-api'));
+        .pipe(replace(/;$/, ""))
+        .pipe(replace(/^\!/, ""))
+        .pipe(gulp.dest("modules/fuse-box-loader-api"));
 
 });
 
-gulp.task('dist-cdn-loader-js', () => {
-    return gulp.src('src/loader/LoaderAPI.ts')
-        .pipe(projectLoader()).on('error', onError).js
+gulp.task("dist-cdn-loader-js", () => {
+    return gulp.src("src/loader/LoaderAPI.ts")
+        .pipe(projectLoader()).on("error", onError).js
         .pipe(wrap(`(function(__root__){
 if (__root__["FuseBox"]) return __root__["FuseBox"];
 <%= contents %>
 return __root__["FuseBox"] = FuseBox; } )(this)`))
-        .pipe(rename('fusebox.js'))
-        .pipe(gulp.dest('modules/fuse-box-loader-api'))
-        .pipe(rename('fusebox.min.js'))
+        .pipe(rename("fusebox.js"))
+        .pipe(gulp.dest("modules/fuse-box-loader-api"))
+        .pipe(rename("fusebox.min.js"))
         .pipe(uglify())
-        .pipe(gulp.dest('dist/'));
+        .pipe(gulp.dest("dist/"));
 
 });
 
-gulp.task('dist-loader-typings', () => {
-    return gulp.src('src/loader/LoaderAPI.ts')
+gulp.task("dist-loader-typings", () => {
+    return gulp.src("src/loader/LoaderAPI.ts")
         .pipe(projectLoaderTypings()).dts
-        .pipe(rename('LoaderAPI.ts'))
-        .pipe(gulp.dest('src/modules/fuse-loader'));
+        .pipe(rename("LoaderAPI.ts"))
+        .pipe(gulp.dest("src/modules/fuse-loader"));
 });
-gulp.task('dist-loader', ['dist-loader-js', 'dist-loader-typings']);
+gulp.task("dist-loader", ["dist-loader-js", "dist-loader-typings"]);
 
 /**
  * Used to build the fusebox modules
  * When adding a new module here be sure to .gitignore `modules/${name}/`
  */
-gulp.task('dist-modules', ['dist-loader-typings'], () => {
+gulp.task("dist-modules", ["dist-loader-typings"], () => {
     return gulp.src(`src/modules/**/*.ts`)
-        .pipe(projectModule()).on('error', onError)
+        .pipe(projectModule()).on("error", onError)
         .pipe(gulp.dest(`modules`));
 });
 
 /**
  * Main building
  */
-gulp.task('dist-typings', () => {
+gulp.task("dist-typings", () => {
     return result = gulp.src(filesMain)
         .pipe(projectTypings()).dts
-        .pipe(gulp.dest('dist/typings'));
+        .pipe(gulp.dest("dist/typings"));
 });
-gulp.task('dist-commonjs', () => {
+gulp.task("dist-commonjs", () => {
     return gulp.src(filesMain)
         .pipe(sourcemaps.init())
-        .pipe(projectCommonjs()).on('error', onError).js
-        .pipe(gulp.dest('dist/commonjs'));
+        .pipe(projectCommonjs()).on("error", onError).js
+        .pipe(gulp.dest("dist/commonjs"));
 });
-gulp.task('dist-main', ['dist-typings', 'dist-commonjs']);
+gulp.task("dist-main", ["dist-typings", "dist-commonjs"]);
 
 /**
  *   NPM deploy management
  */
-gulp.task('publish', ['dist-cdn-loader-js'], function(done) {
-    runSequence('dist', 'increment-version', 'commit-release', 'npm-publish', done);
+gulp.task("publish", ["dist-cdn-loader-js"], function(done) {
+    runSequence("dist", "increment-version", "commit-release", "npm-publish", done);
 });
-gulp.task('changelog', function(done) {
+gulp.task("changelog", function(done) {
     var config = {
-        username: '',
-        password: '',
-        repoOwner: 'fuse-box',
-        repoName: 'fuse-box',
+        username: "",
+        password: "",
+        repoOwner: "fuse-box",
+        repoName: "fuse-box",
     };
-    gulp.src('./docs/changelog.md', { buffer: false, base: './' })
+    gulp.src("./docs/changelog.md", { buffer: false, base: "./" })
         .pipe(clean())
         .pipe(changelog.gulpChangeLogGeneratorPlugin(config))
-        .pipe(gulp.dest('./'))
+        .pipe(gulp.dest("./"))
         .pipe(done);
 });
-gulp.task('increment-version', function() {
-    return gulp.src('./package.json')
+gulp.task("increment-version", function() {
+    return gulp.src("./package.json")
         .pipe(bump())
-        .pipe(gulp.dest('./'));
+        .pipe(gulp.dest("./"));
 });
-gulp.task('commit-release', function(done) {
-    let json = JSON.parse(fs.readFileSync(__dirname + '/package.json').toString());
+gulp.task("commit-release", function(done) {
+    let json = JSON.parse(fs.readFileSync(__dirname + "/package.json").toString());
     child_process.exec(`git add .; git commit -m "Release ${json.version}" -a; git tag v${json.version}; git push origin master --tags`, (error, stdout, stderr) => {
         if (error) {
             console.error(`exec error: ${error}`);
@@ -148,76 +146,75 @@ gulp.task('commit-release', function(done) {
         done();
     });
 });
-gulp.task('npm-publish', function(done) {
-    var publish = spawn('npm', ['publish'], {
-        stdio: 'inherit',
+gulp.task("npm-publish", function(done) {
+    var publish = spawn("npm", ["publish"], {
+        stdio: "inherit",
     });
-    publish.on('close', function(code) {
+    publish.on("close", function(code) {
         if (code === 8) {
-            gulp.log('Error detected, waiting for changes...');
+            gulp.log("Error detected, waiting for changes...");
         }
         done();
     });
 });
 
 
-gulp.task('make-test-runner', (done) => {
-    const { FuseBox, JSONPlugin } = require('./dist/commonjs/index');
-    const version = require('./package.json').version;
+gulp.task("make-test-runner", (done) => {
+    const { FuseBox, JSONPlugin } = require("./dist/commonjs/index");
+    const version = require("./package.json").version;
     FuseBox.init({
         package: {
-            name: 'fuse-box4-test',
-            main: 'index.js',
+            name: "fuse-box4-test",
+            main: "index.js",
         },
         plugins: [JSONPlugin()],
-        homeDir: 'src',
-        outFile: './bin.js',
+        homeDir: "src",
+        outFile: "./bin.js",
         cache: false,
     }).bundle(`[index.ts] +fuse-test-runner +fuse-test-reporter`, done);
 });
 /**
  * Combined build task
  */
-gulp.task('dist', ['dist-main', 'dist-loader', 'dist-modules']);
+gulp.task("dist", ["dist-main", "dist-loader", "dist-modules"]);
 
 /**
  * For development workflow
  */
-gulp.task('watch', ['dist'], function() {
+gulp.task("watch", ["dist"], function() {
     watching = true;
 
-    gulp.watch(['src/loader/**/*.ts'], () => {
-        runSequence('dist-loader');
+    gulp.watch(["src/loader/**/*.ts"], () => {
+        runSequence("dist-loader");
     });
 
-    gulp.watch(['src/modules/**/*.ts'], () => {
-        runSequence('dist-modules');
+    gulp.watch(["src/modules/**/*.ts"], () => {
+        runSequence("dist-modules");
     });
 
     gulp.watch(filesMain, () => {
-        runSequence('dist-main');
+        runSequence("dist-main");
     });
 });
 
-// "lint-modules": "modules/**/*.js"
-gulp.task('installDevDeps', function(done) {
+gulp.task("installDevDeps", function(done) {
     var deps = [
-        'babel-core',
-        'babel-generator',
-        'babel-preset-latest',
-        'babylon',
-        'cheerio',
-        '@angular/core',
-        'stylus',
-        'less',
-        'postcss',
-        'node-sass',
-        'uglify-js',
-        'source-map',
-        'coffee-script',
-        '@types/node',
+        "babel-core",
+        "babel-generator",
+        "babel-preset-latest",
+        "babylon",
+        "cheerio",
+        "@angular/core",
+        "stylus",
+        "less",
+        "postcss",
+        "node-sass",
+        "uglify-js",
+        "source-map",
+        "coffee-script",
+        "@types/node",
     ];
-    var installDeps = spawn('npm', ['install'].concat(deps), {
-        stdio: 'inherit',
+    var installDeps = spawn("npm", ["install"].concat(deps), {
+        stdio: "inherit",
     });
 });
