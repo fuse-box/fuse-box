1f3e6ff463f8b342c98d1fa7cfc8d51ef6dcfa6f
Arete Code <aretecode@gmail.com>
2017-03-09 01:30:58 -0800

    remove built cli

--- bin/cli.js
+++
@@ -1,5 +0,0 @@
-const path = require("path");
-const root = path.join(__dirname, "../");
-process.env.FUSEBOX_DIST_ROOT = root;
-process.env.PROJECT_NODE_MODULES = path.join(root, 'node_modules');
-require("./cli.dev.js");
