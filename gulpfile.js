44e3592a6b6a1b44bad9b1a57b1427191e1b66f4
Ivan.Orlov <ivan@morrr.com>
2017-03-14 13:15:24 +0100

    cache-fixes

--- gulpfile.js
+++ gulpfile.js
@@ -189,11 +189,11 @@ gulp.task("make-test-runner", (done) => {
 });
 
 gulp.task("copy-to-random", () => {
-    return gulp.src("dist/**/**.**")
-        .pipe(gulp.dest("../random/fusemob-ssr/node_modules/fuse-box/dist/"))
+    //return gulp.src("dist/**/**.**")
+    //    .pipe(gulp.dest("../random/fusemob-ssr/node_modules/fuse-box/dist/"))
 });
 gulp.task("copy-api-to-random", () => {
-    return gulp.src("modules/fuse-box-loader-api/**/**.js").pipe(gulp.dest("../random/fusemob-ssr/node_modules/fuse-box/modules/fuse-box-loader-api"))
+    //return gulp.src("modules/fuse-box-loader-api/**/**.js").pipe(gulp.dest("../random/fusemob-ssr/node_modules/fuse-box/modules/fuse-box-loader-api"))
 });
 
 /**
