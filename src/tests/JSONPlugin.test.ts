75beb509afb0b043243302f2b347801ebd110bdb
Feras Khoursheed <ferasman@gmail.com>
2017-03-15 19:07:21 +0400

    Add JSONPlugin test.

--- src/tests/JSONPlugin.test.ts
+++ src/tests/JSONPlugin.test.ts
@@ -1,72 +1,25 @@
 import {createEnv} from "./stubs/TestEnvironment";
 import {should} from "fuse-test-runner";
-import {HTMLPlugin} from "../plugins/HTMLplugin";
+import {JSONPlugin} from "../plugins/JSONplugin";
 
-export class HtmlPluginTest {
-    "Should get Template with Default as export"() {
+export class JSONPluginTest {
+    "Should import JSON file as object"() {
         return createEnv({
             project: {
                 files: {
-                    "index.ts": `const template= require('./index.html');`,
-                    "index.html": `<h1>hello</h1>`,
+                    "index.ts": `const json= require('./config.json');`,
+                    "config.json": `{
+                    "name":"test",
+                    "tags":["fusebox","test"]
+                    }`,
                 },
-                plugins: [HTMLPlugin()],
-                instructions: "> index.ts **/*.html",
+                plugins: [JSONPlugin()],
+                instructions: "> index.ts",
             },
         }).then((result) => {
-            const contents = result.projectContents.toString();
-            should(contents).findString(`module.exports.default =  "<h1>hello</h1>"`);
-        });
-    }
-
-    "Should get Template without Default as export"() {
-        return createEnv({
-            project: {
-                files: {
-                    "index.ts": `const template= require('./index.html');`,
-                    "index.html": `<h1>hello</h1>`,
-                },
-                plugins: [HTMLPlugin({useDefault: false})],
-                instructions: "> index.ts **/*.html",
-            },
-        }).then((result) => {
-            const contents = result.projectContents.toString();
-            should(contents).findString(`module.exports =  "<h1>hello</h1>"`);
-        });
-    }
-
-    "Should import template with Default as export"() {
-        return createEnv({
-            project: {
-                files: {
-                    "index.ts": `const template= require('./index.html');`,
-                    "index.html": `<h1>hello</h1>`,
-                },
-                plugins: [HTMLPlugin()],
-                instructions: "> index.ts **/*.html",
-            },
-        }).then((result) => {
-            const out = result.project.FuseBox.import("./index.html");
-            should(out).deepEqual({
-                    "default": "<h1>hello</h1>"
-                }
+            const out = result.project.FuseBox.import("./config.json");
+            should(out).deepEqual({"name":"test","tags":["fusebox","test"]}
             );
-        });
-    }
-
-    "Should import template without Default as export"() {
-        return createEnv({
-            project: {
-                files: {
-                    "index.ts": `const template= require('./index.html');`,
-                    "index.html": `<h1>hello</h1>`,
-                },
-                plugins: [HTMLPlugin({useDefault: false})],
-                instructions: "> index.ts **/*.html",
-            },
-        }).then((result) => {
-            const out = result.project.FuseBox.import("./index.html");
-            should(out).equal("<h1>hello</h1>");
         });
     }
 }
