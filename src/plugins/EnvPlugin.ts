4cc8903f11ac2e477803fe80f1cfe6b1dd28e36c
Alex Johnson <home@alex-johnson.net>
2017-05-01 01:39:20 -0700

    Revert "add process shim for server bundles"
    This reverts commit 503bb7d8af8fdbc110726921b30e2be0eb337a04.

--- src/plugins/EnvPlugin.ts
+++ src/plugins/EnvPlugin.ts
@@ -1,6 +1,3 @@
-import * as fs from "fs";
-import * as path from "path";
-import { Config } from "./../Config"
 import { WorkFlowContext } from "../core/WorkflowContext";
 import { Plugin } from "../core/WorkflowContext";
 
@@ -17,10 +14,6 @@ export class EnvPluginClass implements Plugin {
     constructor(private env: EnvPluginOptions) { }
     public bundleStart(context: WorkFlowContext) {
         context.source.addContent(`var __process_env__ = ${JSON.stringify(this.env)};`);
-        if (context.serverBundle) {
-            let lib = path.join(Config.FUSEBOX_MODULES, "process", "index.js");
-            context.source.addContent(fs.readFileSync(lib).toString());
-        }
     }
 }
 
