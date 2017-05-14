426a6d9e89bce5f2a2664bd2b446132797f2e303
Ivan.Orlov <ivan@morrr.com>
2017-05-15 00:48:46 +0200

    revert the fix

--- src/BundleSource.ts
+++ src/BundleSource.ts
@@ -101,7 +101,7 @@ export class BundleSource {
     public endCollection(collection: ModuleCollection) {
         let entry = collection.entryFile ? collection.entryFile.info.fuseBoxPath : "";
 
-        if (entry && collection.acceptFiles) {
+        if (entry) {
             this.collectionSource.add(null, `return ___scope___.entry = "${entry}";`);
         }
         this.collectionSource.add(null, "});");
