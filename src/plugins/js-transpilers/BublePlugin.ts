a0dc1beafcfbcda087306df69b16401e440bd087
Arete Code <aretecode@gmail.com>
2017-05-15 07:16:04 -0700

    👕⚒ prettier autofix <3

--- src/plugins/js-transpilers/BublePlugin.ts
+++ src/plugins/js-transpilers/BublePlugin.ts
@@ -11,85 +11,96 @@ let bubleCore;
  * @implements {Plugin}
  */
 export class BublePluginClass implements Plugin {
-    public test: RegExp = /\.(j|t)s(x)?$/;
-    public context: WorkFlowContext;
-    private config?: any = {};
-    private configPrinted = false;
+  /**
+   * We can add tsx and ts here as well
+   * Because Babel won't capture it just being a Plugin
+   * Typescript files are handled before any external plugin is executed
+   */
+  public test: RegExp = /\.(j|t)s(x)?$/;
+  public context: WorkFlowContext;
+  private limit2project: boolean = true;
 
-    /**
-     * @param {any} config BubleOptions + .test
-     */
-    constructor(config: any) {
-        this.config = config || {};
+  private config?: any = {};
+  private configPrinted = false;
+  private configLoaded = false;
 
-        // so we do not pass it to buble, yet can use a flat config
-        if (config.test !== undefined) {
-            this.test = config.test;
-            delete config.test;
-        }
+  /**
+   * @param {any} config BubleOptions + .test
+   */
+  constructor(config: any) {
+    this.config = config || {};
+
+    // so we do not pass it to buble, yet can use a flat config
+    if (config.test !== undefined) {
+      this.test = config.test;
+      delete config.test;
+    }
+  }
+
+  /**
+   * @param {WorkFlowContext} context
+   */
+  public init(context: WorkFlowContext) {
+    this.context = context;
+    context.allowExtension('.jsx');
+  }
+
+  /**
+   * @param {File} file
+   */
+  public transform(file: File, ast: any) {
+    if (!bubleCore) {
+      bubleCore = require('buble');
+    }
+    if (this.configPrinted === false && this.context.doLog === true) {
+      file.context.debug(
+        'BublePlugin',
+        `\n\tConfiguration: ${JSON.stringify(this.config)}`,
+      );
+      this.configPrinted = true;
     }
 
-    /**
-     * @param {WorkFlowContext} context
-     */
-    public init(context: WorkFlowContext) {
-        this.context = context;
-        context.allowExtension(".jsx");
-    }
-
-    /**
-     * @param {File} file
-     */
-    public transform(file: File, ast: any) {
-        if (!bubleCore) {
-            bubleCore = require("buble");
-        }
-        if (this.configPrinted === false && this.context.doLog === true) {
-            file.context.debug("BublePlugin", `\n\tConfiguration: ${JSON.stringify(this.config)}`);
-            this.configPrinted = true;
-        }
-
-        if (this.context.useCache) {
-            if (file.loadFromCache()) {
-                return;
-            }
-        }
-
-        let result;
-        try {
-            const config = {
-              ...this.config,
-              output: file.info.fuseBoxPath,
-              source: file.info.absPath,
-            }
-            result = bubleCore.transform(file.contents, config);
-        } catch (e) {
-            file.analysis.skip();
-            console.error(e);
-            return;
-        }
-
-        if (result.ast) {
-            file.analysis.loadAst(result.ast);
-            let sourceMaps = result.map;
-            file.contents = result.code;
-            file.analysis.analyze();
-
-            if (sourceMaps) {
-                sourceMaps.file = file.info.fuseBoxPath;
-                sourceMaps.sources = [file.info.fuseBoxPath];
-                file.sourceMap = JSON.stringify(sourceMaps);
-            }
-
-            if (this.context.useCache) {
-                this.context.emitJavascriptHotReload(file);
-                this.context.cache.writeStaticCache(file, file.sourceMap);
-            }
-        }
+    if (this.context.useCache) {
+      if (file.loadFromCache()) {
+        return
       }
     }
+
+    let result;
+
+    try {
+      const config = {
+        ...this.config,
+        output: file.info.fuseBoxPath,
+        source: file.info.absPath,
+      }
+      result = bubleCore.transform(file.contents, config);
+    } catch (e) {
+      file.analysis.skip();
+      console.error(e);
+      return
+    }
+
+    if (result.ast) {
+      file.analysis.loadAst(result.ast);
+      let sourceMaps = result.map;
+      file.contents = result.code;
+      file.analysis.analyze();
+
+      if (sourceMaps) {
+        sourceMaps.file = file.info.fuseBoxPath;
+        sourceMaps.sources = [file.info.fuseBoxPath];
+        file.sourceMap = JSON.stringify(sourceMaps);
+      }
+
+      if (this.context.useCache) {
+        this.context.emitJavascriptHotReload(file);
+        this.context.cache.writeStaticCache(file, file.sourceMap);
+      }
+    }
+  }
 }
 
 export const BublePlugin = (opts: any) => {
-    return new BublePluginClass(opts);
+  return new BublePluginClass(opts);
 };
