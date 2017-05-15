367d8b4a043af7dae59d678224810feb8f8803a0
Arete Code <aretecode@gmail.com>
2017-05-15 01:59:56 -0700

    👕🛁 clean unused path & fs for ts

--- src/plugins/js-transpilers/BublePlugin.ts
+++ src/plugins/js-transpilers/BublePlugin.ts
@@ -1,5 +1,3 @@
-import * as fs from "fs";
-import * as path from "path";
 import { File } from "../../core/File";
 import { WorkFlowContext } from "../../core/WorkflowContext";
 import { Plugin } from "../../core/WorkflowContext";
