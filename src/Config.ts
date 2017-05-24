b0d2a39fdae5795c1c034b7b7056f0aed29677fd
pietrorosa77 <pietrorosa77@gmail.com>
2017-05-24 12:13:20 +0100

    possibility to configure the temp folder (#565)
    I had an issue that I can't write inside our source tree (due to a stupid build system we have) so I can't use fusebox unless we have the possibility to configure the fusebox TEMP folder

--- src/Config.ts
+++ src/Config.ts
@@ -10,7 +10,7 @@ if (MAIN_FILE.indexOf("gulp.js") > -1 && !process.env.PROJECT_ROOT) {
 export class Configuration {
     public NODE_MODULES_DIR = process.env.PROJECT_NODE_MODULES || path.join(appRoot.path, "node_modules");
     public FUSEBOX_MODULES = path.join(PROJECT_ROOT, "modules");
-    public TEMP_FOLDER = path.join(appRoot.path, ".fusebox");
+    public TEMP_FOLDER = process.env.FUSEBOX_TEMP_FOLDER || path.join(appRoot.path, ".fusebox");
     public PROJECT_FOLDER = appRoot.path;
     public PROJECT_ROOT = process.env.PROJECT_ROOT || path.dirname(MAIN_FILE);
     public FUSEBOX_VERSION = process.env.FUSEBOX_VERSION || require(path.join(PROJECT_ROOT, "package.json")).version;
