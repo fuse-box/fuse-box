3994e064a97ae0bf36211e0249107819acf08969
Ivan.orlov <nchanged@gmail.com>
2017-10-06 15:02:30 +0200

    Release 2.3.4-beta.3

--- modules/fuse-heresy-default/index.js
+++ modules/fuse-heresy-default/index.js
@@ -1,5 +1,4 @@
 module.exports = function(input) {
-    console.log(input);
     return ['function', 'object', 'array']
         .indexOf(typeof input) > -1 && input.default === undefined ? 
              Object.isFrozen(input) ? input.default = input : Object.defineProperty(input, "default", {value : input, enumerable : false}) : void 0;
