9b291533ba73349b3469cf843832c80c4eb70cee
Unknown <nchanged@gmail.com>
2017-11-30 15:44:14 +0100

    fix: Sparky shortcut functions lose context

--- src/sparky/index.ts
+++ src/sparky/index.ts
@@ -1,7 +1,7 @@
 import { Sparky } from './Sparky';
 import { SparkyContext } from './SparkyContext';
 
-export const src = Sparky.src;
-export const watch = Sparky.watch;
-export const task = Sparky.task;
-export const context = SparkyContext;
+export const src = Sparky.src.bind(Sparky);
+export const watch = Sparky.watch.bind(Sparky);
+export const task = Sparky.task.bind(Sparky);
+export const context = SparkyContext.bind(Sparky);
