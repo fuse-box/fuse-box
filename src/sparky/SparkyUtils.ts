0fd3c73951e1ec8bd4e72e7c140179cc7b859f34
Unknown <nchanged@gmail.com>
2017-12-05 21:49:38 +0100

    chore: Added typings to tsc Sparky

--- src/sparky/SparkyUtils.ts
+++ src/sparky/SparkyUtils.ts
@@ -1,6 +1,102 @@
 import { spawn } from "child_process";
 import * as fs from "fs";
 import { ensureAbsolutePath } from '../Utils';
+export interface TscOptions {
+    allowJs ?: string;
+    allowSyntheticDefaultImports ?: string;
+    allowUnreachableCode ?: string;
+    allowUnusedLabels ?: string;
+    alwaysStrict ?: string;
+    baseUrl ?: string;
+    charset ?: string;
+    checkJs ?: string;
+    declarationDir ?: string;
+    diagnostics ?: string;
+    disableSizeLimit ?: string;
+    downlevelIteration ?: string;
+    emitBOM ?: string;
+    emitDecoratorMetadata ?: string;
+    forceConsistentCasingInFileNames ?: string;
+    importHelpers ?: string;
+    inlineSourceMap ?: string;
+    inlineSources ?: string;
+    isolatedModules ?: string;
+    init ?: string;
+    jsx ?: string;
+    jsxFactory ?: string;
+    lib ?: string[];
+    listEmittedFiles ?: string;
+    listFiles ?: string;
+    locale ?: string;
+    mapRoot ?: string;
+    maxNodeModuleJsDepth ?: string;
+    module ?: string;
+    moduleResolution ?: string;
+    newLine ?: string;
+    noEmit ?: string;
+    noEmitHelpers ?: string;
+    noEmitOnError ?: string;
+    noFallthroughCasesInSwitch ?: string;
+    noImplicitAny ?: string;
+    noImplicitReturns ?: string;
+    noImplicitThis ?: string;
+    noImplicitUseStrict ?: string;
+    noLib ?: string;
+    noResolve ?: string;
+    noStrictGenericChecks ?: string;
+    noUnusedLocals ?: string;
+    noUnusedParameters ?: string;
+    outDir ?: string;
+    outFile ?: string;
+    paths ?: string;
+    preserveConstEnums ?: string;
+    preserveSymlinks ?: string;
+    pretty ?: string;
+    project ?: string;
+    reactNamespace ?: string;
+    removeComments ?: string;
+    rootDir ?: string;
+    skipDefaultLibCheck ?: string;
+    skipLibCheck ?: string;
+    sourceMap ?: string;
+    sourceRoot ?: string;
+    strict ?: string;
+    strictFunctionTypes ?: string;
+    strictNullChecks ?: string;
+    stripInternal ?: string;
+    suppressExcessPropertyErrors ?: string;
+    suppressImplicitAnyIndexErrors ?: string;
+    target ?: string;
+    traceResolution ?: string;
+    types ?: string[];
+    typeRoots ?: string[];
+}
+
+export async function tsc(root: string, opts?: TscOptions) {
+    let tscOptions: any = [];
+    root = ensureAbsolutePath(root);
+    opts.project = root;
+    for (const key in opts) {
+        if( opts[key] !== undefined){
+            tscOptions.push(`--${key}`, opts[key])
+        }
+    }
+
+    return new Promise((resolve, reject) => {
+        const proc = spawn("tsc", tscOptions, {
+            stdio: "inherit"
+        });
+        proc.on("close", function (code) {
+            if (code === 8) {
+                return reject("Error detected");
+            }
+            return resolve();
+        });
+    });
+}
+
+
+
 
 export async function npmPublish(opts: { path: string, tag?: string }) {
     opts.tag = opts.tag || "latest";
