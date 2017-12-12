4697794ea99b935ad329c03dee316d5594be6888
wrench7 <2089588+wrench7@users.noreply.github.com>
2017-12-12 17:10:21 +0000

    Adds border-image CSS prop to CSSResourcePlugin

--- src/lib/postcss/PostCSSResourcePlugin.ts
+++ src/lib/postcss/PostCSSResourcePlugin.ts
@@ -20,7 +20,10 @@ export const PostCSSResourcePlugin = postcss.plugin("css-resource", function (op
         css.walkDecls(declaration => {
             if (declaration.prop) {
 
-                if (declaration.prop.indexOf("background") === 0 || declaration.prop.indexOf("src") === 0 || declaration.prop.indexOf("mask-image") > -1) {
+                if (declaration.prop.indexOf("background") === 0 ||
+                    declaration.prop.indexOf("src") === 0 ||
+                    declaration.prop.indexOf("mask-image") > -1 ||
+                    declaration.prop.indexOf("border-image") > -1) {
                     let re = /url\(([^\)]+)\)/gm;
                     let match;
                     const v = declaration.value;
