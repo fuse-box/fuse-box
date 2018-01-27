7d884cc97ad78bf430fb2a4d85ddfbcff541e5d8
nchanged <nchanged@gmail.com>
2018-01-27 10:08:47 +0100

    fix: allowSyntheticDefaultImports is broken (#1063)
    closes #1063

--- src/loader/LoaderAPI.ts
+++ src/loader/LoaderAPI.ts
@@ -396,7 +396,7 @@ function $trigger(name: string, args: any) {
 function syntheticDefaultExportPolyfill(input){
     return ['function', 'object', 'array']
         .indexOf(typeof input) > -1 && input.default === undefined ?
-             Object.isFrozen(input) ? input.default = input : Object.defineProperty(input, "default", {value : input, enumerable : false}) : void 0;
+             Object.isFrozen(input) ? input.default = input : Object.defineProperty(input, "default", {value : input, writable : true, enumerable : false}) : void 0;
 }
 
 /**
