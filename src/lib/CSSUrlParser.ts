856568e160dece87b1e8b5d0de6c5711a18b1882
Basil A. Zabairatsky <basil@pnml.kz>
2018-02-10 21:36:10 +0600

    fix: data URLs handling in CSSResourcePlugin

--- src/lib/CSSUrlParser.ts
+++ src/lib/CSSUrlParser.ts
@@ -24,7 +24,7 @@ export class CSSUrlParser {
                 return match;
             }
             
-            const replaced = fn(value);
+            const replaced = value && fn(value);
             if (typeof replaced === "string") {
                 return `url("${replaced}")`
             } else {
