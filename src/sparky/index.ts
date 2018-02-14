ca3c810e1411e909477c6e3105eb993f0573c010
Daniel Worthington-Bodart <dan@bodar.com>
2018-02-14 19:58:08 +0000

    Fixes #1101 types missing because of using bind
    See [https://basarat.gitbooks.io/typescript/docs/tips/bind.html] for more details

--- src/sparky/index.ts
+++ src/sparky/index.ts
@@ -2,10 +2,10 @@ import { Sparky } from './Sparky';
 import { SparkyContext } from './SparkyContext';
 
 export { npmPublish, tsc, bumpVersion } from './SparkyUtils';
-export const src = Sparky.src.bind(Sparky);
-export const watch = Sparky.watch.bind(Sparky);
-export const task = Sparky.task.bind(Sparky);
-export const context = SparkyContext.bind(Sparky);
-export const exec = Sparky.exec.bind(Sparky);
-export const init = Sparky.init.bind(Sparky);
-export const fuse = Sparky.fuse.bind(Sparky);
+export const src: typeof Sparky.src = Sparky.src.bind(Sparky);
+export const watch: typeof Sparky.watch  = Sparky.watch.bind(Sparky);
+export const task: typeof Sparky.task  = Sparky.task.bind(Sparky);
+export const context: typeof SparkyContext  = SparkyContext.bind(Sparky);
+export const exec: typeof Sparky.exec  = Sparky.exec.bind(Sparky);
+export const init: typeof Sparky.init  = Sparky.init.bind(Sparky);
+export const fuse: typeof Sparky.fuse  = Sparky.fuse.bind(Sparky);
