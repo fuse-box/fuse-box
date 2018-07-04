8d13bd968c9bbc72ac91f2beb269cdaf8b00fdfa
Christian Todd <christiantodd27@icloud.com>
2018-07-04 15:58:53 -0400

    feat: adjust `pre` option in webindex to a string instead of an object and update docs.

--- src/plugins/WebIndexPlugin.ts
+++ src/plugins/WebIndexPlugin.ts
@@ -1,78 +1,86 @@
-import { } from "../core/File";
-import { Plugin } from "../core/WorkflowContext";
-import { BundleProducer } from "../core/BundleProducer";
-import * as fs from "fs";
-import { ensureAbsolutePath, joinFuseBoxPath } from "../Utils";
-import { UserOutput } from "../core/UserOutput";
-import * as path from "path";
+import * as fs from 'fs';
+import * as path from 'path';
+
+import { } from '../core/File';
+import { Plugin } from '../core/WorkflowContext';
+import { BundleProducer } from '../core/BundleProducer';
+import { UserOutput } from '../core/UserOutput';
+import { ensureAbsolutePath, joinFuseBoxPath } from '../Utils';
 
 export interface IndexPluginOptions {
-    title?: string;
-    charset?: string;
-    description?: string;
-    keywords?: string;
+    appendBundles?: boolean;
+    async?: boolean;
     author?: string;
     bundles?: string[];
+    charset?: string;
+    description?: string;
+    emitBundles?: (bundles: string[]) => string;
+    engine?: string;
+    keywords?: string;
+    locals?: { [key: string]: any }
     path?: string;
+    pre?: string;
+    resolve?: { (output: UserOutput): string };
+    scriptAttributes?: string;
     target?: string;
     template?: string;
     templateString?: string;
-    appendBundles?: boolean;
-    engine?: string;
-    locals?: {[key:string]: any}
-    async?: boolean;
-    scriptAttributes?: string;
-    pre?: { relType: 'fetch' | 'load' };
-    resolve?: { (output: UserOutput): string };
-    emitBundles?: (bundles: string[]) => string;
+    title?: string;
 }
+
+const validPreAttrs = ['fetch', 'load'];
+
 export class WebIndexPluginClass implements Plugin {
     constructor(public opts?: IndexPluginOptions) {
 
     }
 
     private async generate(producer: BundleProducer) {
+        const bundles = producer.sortBundles();
         let bundlePaths = [];
-        let bundles = producer.sortBundles();
-        bundles.forEach((bundle) => {
+
+        bundles.forEach(bundle => {
             let pass = true;
-            if (this.opts.bundles) {
-                if (this.opts.bundles.indexOf(bundle.name) === -1) {
-                    pass = false;
-                }
+
+            if (this.opts.bundles && !this.opts.bundles.includes(bundle.name)) {
+                pass = false;
             }
+
             pass = pass && bundle.webIndexed;
+
             if (pass) {
                 const output = bundle.context.output;
                 if (output && output.lastPrimaryOutput) {
                     if (this.opts.resolve) {
-                        bundlePaths.push(this.opts.resolve(output))
+                        bundlePaths.push(this.opts.resolve(output));
                     } else {
                         bundlePaths.push(
-                            joinFuseBoxPath(this.opts.path ? this.opts.path : "/", output.folderFromBundleName || "/",
+                            joinFuseBoxPath(this.opts.path ? this.opts.path : '/', output.folderFromBundleName || '/',
                                 output.lastPrimaryOutput.filename)
-                        )
+                        );
                     }
                 }
-
             }
         });
 
 
-        let html = this.opts.templateString || `<!DOCTYPE html><html>
-<head>
-<title>$title</title>
-$charset
-$description
-$keywords
-$preload
-$author
-$css
-</head>
-<body>
-$bundles
-</body>
-</html>`;
+        let html = this.opts.templateString
+            || `<!doctype html>
+                <html>
+                    <head>
+                        <title>$title</title>
+                        $charset
+                        $description
+                        $keywords
+                        $pre
+                        $author
+                        $css
+                    </head>
+                <body>
+                    $bundles
+                </body>
+                </html>`;
+
         if (this.opts.engine) {
             const engine = this.opts.engine;
             const consolidate = require('consolidate');
@@ -87,13 +95,13 @@ $bundles
             html = await consolidate[engine](filePath, this.opts.locals || {});
         } else {
             if (this.opts.template) {
-                let filePath = ensureAbsolutePath(this.opts.template);
-                html = fs.readFileSync(filePath).toString();
+                const pathToTemplate = ensureAbsolutePath(this.opts.template);
+                html = fs.readFileSync(pathToTemplate, 'UTF-8')
 
-                if (this.opts.appendBundles && html.indexOf('$bundles') === -1) {
-                    if (html.indexOf('</body>') !== -1) {
+                if (this.opts.appendBundles && !html.includes('$bundles')) {
+                    if (!html.includes('</body>')) {
                         html = html.replace('</body>', '$bundles</body>');
-                    } else if (html.indexOf('</head>') !== -1) {
+                    } else if (!html.includes('</head>')) {
                         html = html.replace('</head>', '$bundles</head>');
                     } else {
                         html = `${html}$bundles`;
@@ -102,40 +110,44 @@ $bundles
             }
         }
 
-        let jsTags = this.opts.emitBundles
+        const jsTags = this.opts.emitBundles
             ? this.opts.emitBundles(bundlePaths)
             : bundlePaths.map(bundle => `<script ${this.opts.async ? 'async' : ''} ${this.opts.scriptAttributes ? this.opts.scriptAttributes : ''} type="text/javascript" src="${bundle}"></script>`).join('\n');
 
-        let preloadTags;
-        if (this.opts.pre) {
-            preloadTags = bundlePaths.map(bundle =>
-                `<link rel="pre${this.opts.pre.relType}" as="script" href="${bundle}">`
-            ).join("\n");
+        let preLinkTags;
+        if (this.opts.pre && validPreAttrs.includes[this.opts.pre]) {
+            preLinkTags = bundlePaths.map(bundle =>
+                `<link rel="pre${this.opts.pre}" as="script" href="${bundle}">`
+            ).join('\n');
         }
+
         let cssInjection = [];
         if (producer.injectedCSSFiles.size > 0) {
-            producer.injectedCSSFiles.forEach(f => {
-                const resolvedFile = this.opts.path ? path.join(this.opts.path, f) : path.join("/", f);
-                cssInjection.push(`<link rel="stylesheet" href="${resolvedFile}"/>`)
-            })
+            producer.injectedCSSFiles.forEach(file => {
+                const resolvedFile = this.opts.path ? path.join(this.opts.path, file) : path.join('/', file);
+                cssInjection.push(`<link rel="stylesheet" href="${resolvedFile}"/>`);
+            });
         }
 
         let macro = {
-            css: cssInjection.join('\n'),
-            title: this.opts.title ? this.opts.title : "",
-            charset: this.opts.charset ? `<meta charset="${this.opts.charset}">` : "",
-            description: this.opts.description ? `<meta name="description" content="${this.opts.description}">` : "",
-            keywords: this.opts.keywords ? `<meta name="keywords" content="${this.opts.keywords}">` : "",
-            author: this.opts.author ? `<meta name="author" content="${this.opts.author}">` : "",
+            author: this.opts.author ? `<meta name="author" content="${this.opts.author}">` : '',
             bundles: jsTags,
-            preload: this.opts.pre ? preloadTags : "",
+            charset: this.opts.charset ? `<meta charset="${this.opts.charset}">` : '',
+            css: cssInjection.join('\n'),
+            description: this.opts.description ? `<meta name="description" content="${this.opts.description}">` : '',
+            keywords: this.opts.keywords ? `<meta name="keywords" content="${this.opts.keywords}">` : '',
+            pre: this.opts.pre ? preLinkTags : '',
+            title: this.opts.title ? this.opts.title : '',
         }
+
         for (let key in macro) {
             html = html.replace('$' + key, macro[key])
         }
+
         producer.fuse.context
-            .output.writeToOutputFolder(this.opts.target || "index.html", html);
+            .output.writeToOutputFolder(this.opts.target || 'index.html', html);
     }
+
     producerEnd(producer: BundleProducer) {
         this.generate(producer);
         producer.sharedEvents.on('file-changed', () => {
