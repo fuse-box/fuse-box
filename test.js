4df949dcc5a90ebd5ed0db92592ff5c65ce6bd4d
Natan Sągol <m@merlinnot.com>
2018-08-14 20:46:52 +0200

    refactor(test.js): Refine test runner and npm scripts.
    - test.js script should be easier to understand
    - test:once script is removed because it's not needed and there are no mocha tests

--- test.js
+++ test.js
@@ -1,18 +1,30 @@
-const path = require("path");
-process.env.FUSEBOX_DIST_ROOT = __dirname;
-process.env.FUSEBOX_VERSION = "2.4.0";
-process.env.PROJECT_NODE_MODULES = path.join(__dirname, "node_modules");
-process.env.SPARKY_LOG = false;
-process.env.LOGGING = false;
-process.env.DYNAMIC_IMPORTS_DISABLED = true;
+const { join } = require("path");
+const { argv, env } = require("process");
 const { FuseBox } = require("./bin/fusebox");
 
-let file = process.argv[2];
-let special = file && file.match(/^--file=(.*)/);
-let mask = "*.test.ts";
-if (special) {
-	mask = special = special[1];
-}
+const defaultTestFileMask = "*.test.ts";
+const projectRoot = __dirname;
+
+/*
+ * Definition of environment variables required to run tests in the node
+ * environment.
+ */
+env.DYNAMIC_IMPORTS_DISABLED = true;
+env.FUSE_TEST_TIMEOUT = 5000;
+env.FUSEBOX_DIST_ROOT = projectRoot;
+env.FUSEBOX_VERSION = "2.4.0";
+env.LOGGING = false;
+env.PROJECT_NODE_MODULES = join(projectRoot, "node_modules");
+env.SPARKY_LOG = false;
+
+/*
+ * A --file flag can be used to override a default test mask.
+ */
+const maybeFileFlag = argv[2];
+const fileFlag = typeof maybeFileFlag === "string" && maybeFileFlag.match(/^--file=(.*)/);
+
+const testFileMask = fileFlag === false ? defaultTestFileMask : fileFlag[1];
+
 const fuse = FuseBox.init({
 	homeDir: "src",
 	target: "server@esnext",
@@ -21,4 +33,5 @@ const fuse = FuseBox.init({
 	output: ".fusebox/$name.js",
 	cache: false,
 });
-fuse.bundle("fusebox").test(`[**/${mask}]`);
+
+fuse.bundle("fusebox").test(`[**/${testFileMask}]`);
