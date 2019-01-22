ca96306879882d0b607031065dba036c29a3596b
Eddy Wilson <eddyw@users.noreply.github.com>
2019-01-22 19:10:02 +0800

    feat: add tests checking config files loaded correctly

---
+++ src/tests/plugins/Babel7PluginConfig/plugin-test.js
@@ -0,0 +1,20 @@
+// Helper, check if local plugin is loaded
+module.exports = function testPlugin(api) {
+	api.assertVersion(7);
+	return {
+		name: "plugin-test",
+		visitor: {
+			FunctionDeclaration({ node }) {
+				// Helper, checks whether plugin was called or not successfully
+				node.id.name = "IwasTranspiledWithBabel";
+			},
+			StringLiteral({ node }, state) {
+				// Helper, checks if filename is provided in parser options in Babel7 plugin | Issue #1463
+				if (node.value === "replaceWithFilename") {
+					const { filename } = state.file.opts;
+					node.value = String(filename || '');
+				}
+			}
+		}
+	};
+};
