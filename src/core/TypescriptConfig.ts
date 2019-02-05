00414fcd076b698cfc534031ca4482e87f1f24af
Unknown <nchanged@gmail.com>
2019-02-05 15:28:59 +0100

    fix: tsconfig baseURL and automatic alias

--- src/core/TypescriptConfig.ts
+++ src/core/TypescriptConfig.ts
@@ -18,40 +18,35 @@ const CACHED: { [path: string]: any } = {};
  * e.g:
  * - Instead of `target: 1`, user can input `target: 'ES5'`
  */
-export type rawScriptTarget = Exclude<keyof typeof ts.ScriptTarget, "JSON" | "Latest">
+export type rawScriptTarget = Exclude<keyof typeof ts.ScriptTarget, "JSON" | "Latest">;
 export type rawCompilerOptions = {
-	[key in keyof ts.CompilerOptions]: (
-		key extends "maxNodeModuleJsDepth"
+	[key in keyof ts.CompilerOptions]: key extends "maxNodeModuleJsDepth"
 		? number
 		: ts.CompilerOptions[key] extends ts.ScriptTarget
-		? rawScriptTarget
-		: ts.CompilerOptions[key] extends ts.JsxEmit
-		? "react" | "preserve" | "react-native"
-		: ts.CompilerOptions[key] extends ts.ModuleKind
-		? keyof typeof ts.ModuleKind
-		: ts.CompilerOptions[key] extends ts.ModuleResolutionKind
-		? keyof typeof ts.ModuleResolutionKind
-		: ts.CompilerOptions[key] extends ts.NewLineKind
-		? "CRLF" | "LF"
-		: ts.CompilerOptions[key] extends ts.MapLike<string[]>
-		? ts.MapLike<string[]>
-		: ts.CompilerOptions[key] extends string[]
-		? string[]
-		: ts.CompilerOptions[key] extends string
-		? ts.CompilerOptions[key]
-		: ts.CompilerOptions[key] extends boolean
-		? ts.CompilerOptions[key]
-		: ts.CompilerOptions[key] extends number
-		? number
-		: any
-	)
-}
+			? rawScriptTarget
+			: ts.CompilerOptions[key] extends ts.JsxEmit
+				? "react" | "preserve" | "react-native"
+				: ts.CompilerOptions[key] extends ts.ModuleKind
+					? keyof typeof ts.ModuleKind
+					: ts.CompilerOptions[key] extends ts.ModuleResolutionKind
+						? keyof typeof ts.ModuleResolutionKind
+						: ts.CompilerOptions[key] extends ts.NewLineKind
+							? "CRLF" | "LF"
+							: ts.CompilerOptions[key] extends ts.MapLike<string[]>
+								? ts.MapLike<string[]>
+								: ts.CompilerOptions[key] extends string[]
+									? string[]
+									: ts.CompilerOptions[key] extends string
+										? ts.CompilerOptions[key]
+										: ts.CompilerOptions[key] extends boolean
+											? ts.CompilerOptions[key]
+											: ts.CompilerOptions[key] extends number ? number : any
+};
 
 export function getScriptLevelNumber(level: any): ScriptTarget & number | undefined {
 	if (Number.isNaN(Number(level)) && typeof level === "string") {
-		const key = Object
-			.keys(ScriptTarget)
-			.filter(k => !['json', 'latest'].includes(k.toLowerCase()))
+		const key = Object.keys(ScriptTarget)
+			.filter(k => !["json", "latest"].includes(k.toLowerCase()))
 			.find(t => t.toLowerCase() === level.toLowerCase());
 		return key ? ScriptTarget[key] : undefined;
 	}
@@ -62,29 +57,28 @@ export function getScriptLevelNumber(level: any): ScriptTarget & number | undefi
 
 export function getScriptLevelString(level: any): rawScriptTarget | undefined {
 	if (Number(level) in ScriptTarget) {
-		const key = Object
-			.keys(ScriptTarget)
-			.filter(k => !['json', 'latest'].includes(k.toLowerCase()))
+		const key = Object.keys(ScriptTarget)
+			.filter(k => !["json", "latest"].includes(k.toLowerCase()))
 			.find(t => ScriptTarget[t] === Number(level));
 		return key as rawScriptTarget;
 	}
 }
 
 export interface ICategorizedDiagnostics {
-	errors: ReadonlyArray<ts.Diagnostic & { category: ts.DiagnosticCategory.Error }>,
-	warnings: ReadonlyArray<ts.Diagnostic & { category: ts.DiagnosticCategory.Warning }>,
-	messages: ReadonlyArray<ts.Diagnostic & { category: ts.DiagnosticCategory.Message }>,
-	suggestions: ReadonlyArray<ts.Diagnostic & { category: ts.DiagnosticCategory.Suggestion }>,
+	errors: ReadonlyArray<ts.Diagnostic & { category: ts.DiagnosticCategory.Error }>;
+	warnings: ReadonlyArray<ts.Diagnostic & { category: ts.DiagnosticCategory.Warning }>;
+	messages: ReadonlyArray<ts.Diagnostic & { category: ts.DiagnosticCategory.Message }>;
+	suggestions: ReadonlyArray<ts.Diagnostic & { category: ts.DiagnosticCategory.Suggestion }>;
 }
 
 export interface TSParsedConfig {
-	errors: Array<ts.Diagnostic | ts.DiagnosticWithLocation> | ts.SortedReadonlyArray<ts.Diagnostic>,
-	compilerOptions: ts.CompilerOptions,
-	compileOnSave?: boolean,
-	projectReferences?: ReadonlyArray<ts.ProjectReference>,
-	raw?: rawCompilerOptions,
-	typeAcquisition?: ts.TypeAcquisition,
-	wildcardDirectories?: ts.MapLike<ts.WatchDirectoryFlags>,
+	errors: Array<ts.Diagnostic | ts.DiagnosticWithLocation> | ts.SortedReadonlyArray<ts.Diagnostic>;
+	compilerOptions: ts.CompilerOptions;
+	compileOnSave?: boolean;
+	projectReferences?: ReadonlyArray<ts.ProjectReference>;
+	raw?: rawCompilerOptions;
+	typeAcquisition?: ts.TypeAcquisition;
+	wildcardDirectories?: ts.MapLike<ts.WatchDirectoryFlags>;
 }
 
 export const IGNORED_DIAGNOSTICS = new Set([
@@ -100,7 +94,7 @@ export function makePathDoesNotExistDiagnostic(path: string): ts.Diagnostic {
 		file: undefined,
 		length: undefined,
 		start: undefined,
-	}
+	};
 }
 
 export class TypescriptConfig {
@@ -114,7 +108,7 @@ export class TypescriptConfig {
 			getCanonicalFileName: file => file,
 			getCurrentDirectory: () => context.homeDir,
 			getNewLine: () => ts.sys.newLine,
-		}
+		};
 	}
 
 	public getConfig() {
@@ -123,7 +117,7 @@ export class TypescriptConfig {
 	}
 
 	public normalizeDiagnostics(diagnostics: ts.Diagnostic[]): ts.SortedReadonlyArray<ts.Diagnostic> {
-		return ts.sortAndDeduplicateDiagnostics(diagnostics.filter(x => !IGNORED_DIAGNOSTICS.has(x.code)))
+		return ts.sortAndDeduplicateDiagnostics(diagnostics.filter(x => !IGNORED_DIAGNOSTICS.has(x.code)));
 	}
 
 	private findConfigFileBackwards(tsConfigFilePath: string): string {
@@ -133,16 +127,15 @@ export class TypescriptConfig {
 	public readJsonConfigFile(): TSParsedConfig {
 		const config: TSParsedConfig = { compilerOptions: {}, errors: [] };
 
-		this.configFile = (
+		this.configFile =
 			typeof this.customTsConfig === "string"
 				? ensureUserPath(this.customTsConfig)
-				: this.findConfigFileBackwards(path.join(this.context.homeDir, "tsconfig.json"))
-		);
+				: this.findConfigFileBackwards(path.join(this.context.homeDir, "tsconfig.json"));
 
 		if (this.configFile) {
 			if (!ts.sys.fileExists(this.configFile)) {
 				config.errors = [makePathDoesNotExistDiagnostic(this.configFile)];
-				return config
+				return config;
 			}
 			const configFileRelPath = this.configFile.replace(this.context.appRoot, "");
 			this.context.log.echoInfo(`Typescript config file:  ${configFileRelPath}`);
@@ -167,7 +160,7 @@ export class TypescriptConfig {
 				JSONSourceFile,
 				ts.sys,
 				path.dirname(this.configFile),
-			)
+			);
 			if (parsedJSONConfigFile.errors.length) {
 				const errors = this.normalizeDiagnostics(parsedJSONConfigFile.errors);
 				if (errors.length) {
@@ -176,41 +169,34 @@ export class TypescriptConfig {
 				}
 			}
 
-			Object.assign(
-				config,
-				{
-					compilerOptions: parsedJSONConfigFile.options,
-					compileOnSave: parsedJSONConfigFile.compileOnSave,
-					projectReferences: parsedJSONConfigFile.projectReferences,
-					raw: parsedJSONConfigFile.raw,
-					typeAcquisition: parsedJSONConfigFile.typeAcquisition,
-					wildcardDirectories: parsedJSONConfigFile.wildcardDirectories,
-				},
-			);
+			Object.assign(config, {
+				compilerOptions: parsedJSONConfigFile.options,
+				compileOnSave: parsedJSONConfigFile.compileOnSave,
+				projectReferences: parsedJSONConfigFile.projectReferences,
+				raw: parsedJSONConfigFile.raw,
+				typeAcquisition: parsedJSONConfigFile.typeAcquisition,
+				wildcardDirectories: parsedJSONConfigFile.wildcardDirectories,
+			});
 		}
 
 		if (Array.isArray(this.customTsConfig)) {
 			const tsConfigOverrideCompilerOptions = {};
 			this.customTsConfig.forEach(config => {
-				if (
-					typeof config === "object" &&
-					config !== null
-				) {
+				if (typeof config === "object" && config !== null) {
 					Object.assign(tsConfigOverrideCompilerOptions, config);
-				} else { /** TODO! unknown type */ }
+				} else {
+					/** TODO! unknown type */
+				}
 			});
 
 			const parsedVirtualJSONConfig = ts.convertCompilerOptionsFromJson(
 				tsConfigOverrideCompilerOptions,
 				config.compilerOptions.baseUrl || ".",
-				""
+				"",
 			);
-			const virtualJSONSourceFile = ts.parseJsonText(
-				"[FuseBoxOptions.tsConfig]",
-				JSON.stringify(this.customTsConfig)
-			);
+			const virtualJSONSourceFile = ts.parseJsonText("[FuseBoxOptions.tsConfig]", JSON.stringify(this.customTsConfig));
 
-			const errors = this.normalizeDiagnostics(parsedVirtualJSONConfig.errors)
+			const errors = this.normalizeDiagnostics(parsedVirtualJSONConfig.errors);
 			if (errors.length) {
 				const flattenError = errors[0];
 				flattenError.file = virtualJSONSourceFile;
@@ -219,7 +205,7 @@ export class TypescriptConfig {
 
 				for (let i = 1; i < errors.length; i++) {
 					const error = errors[i];
-					flattenError.messageText += '\n' + this.formatDiagnostic(error, false);
+					flattenError.messageText += "\n" + this.formatDiagnostic(error, false);
 				}
 
 				config.errors = [flattenError];
@@ -241,14 +227,14 @@ export class TypescriptConfig {
 			this.context.fuse.producer.allowSyntheticDefaultImports = config.compilerOptions.allowSyntheticDefaultImports;
 		}
 
-		return config
+		return config;
 	}
 
 	private defaultSetup() {
 		const compilerOptions = (this.config.compilerOptions = this.config.compilerOptions || {});
 
-		if (this.configFile && path.basename(this.configFile).startsWith('jsconfig.')) {
-			compilerOptions.allowJs = true
+		if (this.configFile && path.basename(this.configFile).startsWith("jsconfig.")) {
+			compilerOptions.allowJs = true;
 		}
 		compilerOptions.sourceMap = this.context.useSourceMaps;
 		compilerOptions.inlineSources = this.context.useSourceMaps;
@@ -257,10 +243,15 @@ export class TypescriptConfig {
 			this.forceCompilerTarget(this.context.forcedLanguageLevel);
 		}
 
-		if (compilerOptions.baseUrl === "." && this.context.automaticAlias) {
+		const isValidBaseURL = compilerOptions.baseUrl === "." || compilerOptions.baseUrl === this.context.homeDir;
+		if (isValidBaseURL && this.context.automaticAlias) {
 			let aliasConfig = {};
 			let log = [];
 			fs.readdirSync(this.context.homeDir).forEach(file => {
+				// skip files that start with .
+				if (file[0] === ".") {
+					return;
+				}
 				const extension = path.extname(file);
 				if (!extension || extension === ".ts" || extension === ".tsx") {
 					let name = file;
@@ -291,11 +282,11 @@ export class TypescriptConfig {
 		if (!this.configFile && this.context.ensureTsConfig === true) {
 			Object.assign(this.config.compilerOptions, {
 				jsx: ts.JsxEmit.React,
-				baseUrl: '.',
+				baseUrl: ".",
 				importHelpers: true,
 				emitDecoratorMetadata: true,
 				experimentalDecorators: true,
-			})
+			});
 			// Raw compiler options
 			const compilerOptions: rawCompilerOptions = Object.assign({}, this.config.compilerOptions as any, {
 				target: getScriptLevelString(this.config.compilerOptions.target),
@@ -319,14 +310,14 @@ export class TypescriptConfig {
 		}
 	}
 
-	public categorizeDiagnostics(diagnostics: ReadonlyArray<ts.Diagnostic> | Readonly<ts.Diagnostic>): ICategorizedDiagnostics {
+	public categorizeDiagnostics(
+		diagnostics: ReadonlyArray<ts.Diagnostic> | Readonly<ts.Diagnostic>,
+	): ICategorizedDiagnostics {
 		const errors = [];
 		const warnings = [];
 		const messages = [];
 		const suggestions = [];
-		const diagnosticsArray = Array.isArray(diagnostics)
-			? diagnostics
-			: [diagnostics];
+		const diagnosticsArray = Array.isArray(diagnostics) ? diagnostics : [diagnostics];
 		const size = diagnosticsArray.length;
 		for (let i = 0; i < size; i++) {
 			const diagnostic: ts.Diagnostic = diagnosticsArray[i];
@@ -357,26 +348,24 @@ export class TypescriptConfig {
 
 	public formatDiagnostic(diagnostic: ts.Diagnostic, separator: boolean = true): string {
 		// TODO! make a custom formatter instead
-		diagnostic.messageText = `${diagnostic.messageText}\n`
-		const formatted = ts.formatDiagnosticsWithColorAndContext(
-			[diagnostic],
-			this.formatDiagnosticsHost,
-		)
+		diagnostic.messageText = `${diagnostic.messageText}\n`;
+		const formatted = ts
+			.formatDiagnosticsWithColorAndContext([diagnostic], this.formatDiagnosticsHost)
 			.replace(/error|warning|message/, match => match.toUpperCase())
-			.replace(/ - /, '\n')
-			.split('\n')
+			.replace(/ - /, "\n")
+			.split("\n")
 			.filter(m => String(m).trim().length)
-			.map((line, i) => i === 0 ? line : separator ? `  │ ${line}` : line)
-			.join('\n')
+			.map((line, i) => (i === 0 ? line : separator ? `  │ ${line}` : line))
+			.join("\n");
 
-		return formatted
+		return formatted;
 	}
 
 	public logDiagnosticsByCategory(diagnostics: ReadonlyArray<ts.Diagnostic>, category: ts.DiagnosticCategory): void {
 		if (diagnostics.length) {
-			const size = diagnostics.length
+			const size = diagnostics.length;
 			for (let i = 0; i < size; i++) {
-				const diagnosticMsg = this.formatDiagnostic(diagnostics[i])
+				const diagnosticMsg = this.formatDiagnostic(diagnostics[i]);
 				switch (category) {
 					case ts.DiagnosticCategory.Error: {
 						this.context.log.echoRed(`  → ${diagnosticMsg}`);
@@ -414,11 +403,10 @@ export class TypescriptConfig {
 	}
 
 	public read() {
-		const cacheKey = (
-			typeof this.customTsConfig === "string"
-				? this.customTsConfig
-				: this.context.homeDir
-		) + this.context.target + this.context.languageLevel;
+		const cacheKey =
+			(typeof this.customTsConfig === "string" ? this.customTsConfig : this.context.homeDir) +
+			this.context.target +
+			this.context.languageLevel;
 
 		if (CACHED[cacheKey]) {
 			this.config = CACHED[cacheKey];
