39917177f775033f7632b57f4902697e237f0683
Eddy Wilson <eddyw@users.noreply.github.com>
2019-02-25 18:47:21 +0800

    test: add more tests for paths cases

--- src/_v4/__tests__/resolver/pathsLookup.test.ts
+++ src/_v4/__tests__/resolver/pathsLookup.test.ts
@@ -54,7 +54,7 @@ describe("Paths lookup", () => {
 			expect(result.absPath).toMatchFilePath("something/app/Foo.tsx$");
 		});
 
-		it("should look up an find in 2 directories", () => {
+		it("should look up and find in 2 directories", () => {
 			const result = pathsLookup({
 				baseURL: CASE1,
 				homeDir: CASE1,
@@ -66,6 +66,62 @@ describe("Paths lookup", () => {
 			expect(result.fileExists).toBe(true);
 			expect(result.extension).toEqual(".jsx");
 			expect(result.absPath).toMatchFilePath("something/other/AnotherFile.jsx$");
+		});
+
+		it("should look up and find @app/*/foo (middle star)", () => {
+			const result = pathsLookup({
+				baseURL: CASE1,
+				homeDir: CASE1,
+				paths: {
+					"@app/*/foo": ["something/*/Foo"],
+				},
+				target: "@app/app/foo",
+			});
+			expect(result.fileExists).toBe(true);
+			expect(result.extension).toEqual(".tsx");
+			expect(result.absPath).toMatchFilePath("something/app/Foo.tsx$");
+		});
+
+		it("should look up and find @app/* (key with star | dir no star)", () => {
+			const result = pathsLookup({
+				baseURL: CASE1,
+				homeDir: CASE1,
+				paths: {
+					"@app/*": ["foo"],
+				},
+				target: "@app/anything",
+			});
+			expect(result.fileExists).toBe(true);
+			expect(result.extension).toEqual(".ts");
+			expect(result.absPath).toMatchFilePath("foo/index.ts$");
+		});
+
+		it("should look up and find @app* (key with star, no slash)", () => {
+			const result = pathsLookup({
+				baseURL: CASE1,
+				homeDir: CASE1,
+				paths: {
+					"@app*": ["foo"],
+				},
+				target: "@appFindFoo",
+			});
+			expect(result.fileExists).toBe(true);
+			expect(result.extension).toEqual(".ts");
+			expect(result.absPath).toMatchFilePath("foo/index.ts$");
+		});
+
+		it("should look up and find 'path' (alias to foo/index.js)", () => {
+			const result = pathsLookup({
+				baseURL: CASE1,
+				homeDir: CASE1,
+				paths: {
+					path: ["foo"],
+				},
+				target: "path",
+			});
+			expect(result.fileExists).toBe(true);
+			expect(result.extension).toEqual(".ts");
+			expect(result.absPath).toMatchFilePath("foo/index.ts$");
 		});
 	});
 });
