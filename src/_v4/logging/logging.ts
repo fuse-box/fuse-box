263a3aaec66c7504dff25fdacdb65c93e85f57ae
Unknown <nchanged@gmail.com>
2019-02-26 15:55:17 +0100

    chore: added logging dumb interface

---
+++ src/_v4/logging/logging.ts
@@ -0,0 +1,28 @@
+export enum LogLevel {
+	LOG_ALL,
+	LOG_ERRORS,
+	LOG_WARNINGS,
+}
+export interface ILoggerProps {
+	logLevel: LogLevel;
+}
+
+function Spinner() {
+	return {
+		setColor: () => {},
+		setText: () => {},
+		start: () => {},
+		stop: () => {},
+	};
+}
+
+export function getLogger(props: ILoggerProps) {
+	return {
+		info: message => {},
+		warning: message => {},
+		deprecated: message => {},
+		createSpinner: () => {
+			return Spinner();
+		},
+	};
+}
--- src/_v4/resolver/nodeModuleLookup.ts
+++ src/_v4/resolver/nodeModuleLookup.ts
@@ -5,7 +5,11 @@ import * as fs from "fs";
 
 const PROJECT_NODE_MODULES = path.join(appRoot.path, "node_modules");
 
-function traverseUp(fileName: string, limit: string) {}
+function findPackageRoot(fileName: string) {
+	const dir = path.dirname(fileName);
+	const lastIndex = dir.lastIndexOf("node_modules");
+	return dir.substring(0, lastIndex);
+}
 
 function getPrimaryModule(props: IResolverProps) {
 	const targetPath = path.join(PROJECT_NODE_MODULES, props.target);
@@ -35,8 +39,11 @@ export function findTargetFolder(props: IResolverProps, folders?: Array<string>)
 			return targetPath;
 		}
 	} else {
-		console.log(props.filePath);
-		console.log(path.join(props.filePath, "../"));
+		const packageRoot = findPackageRoot(props.filePath);
+		console.log(props.filePath, packageRoot);
+
+		// console.log(props.filePath);
+		// console.log(path.join(props.filePath, "../"));
 		// otherwise we are inside the node_modules, which means we shoudl
 		// traverse back until we find node_modules/[lib] and try finding node_modules underneath it,
 		// otherwise we drop back to the primary node_module
