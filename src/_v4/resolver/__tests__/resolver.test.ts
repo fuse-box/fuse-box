cec3d3e1a197e7ce5b86dc104e177dd6e78e353e
Unknown <nchanged@gmail.com>
2019-03-06 14:15:20 +0100

    feat: new moduleResolver

--- src/_v4/resolver/__tests__/resolver.test.ts
+++ src/_v4/resolver/__tests__/resolver.test.ts
@@ -1,8 +1,45 @@
-import { resolveModule } from "../resolver";
 import * as path from "path";
 import { createRealNodeModule } from "../../test_utils";
-
+import { resolveModule } from "../resolver";
 const cases = path.join(__dirname, "cases/");
+const customModules = path.join(cases, "_modules");
+createRealNodeModule(
+	"resolver-test_a",
+	{
+		main: "index.js",
+		version: "1.0.1",
+		browser: "something-for-browser.js",
+	},
+	{
+		"index.js": "module.exports = {}",
+		"foobar.js": "module.exports = {}",
+		"components/MyComponent.jsx": "",
+		"sub/package.json": JSON.stringify({
+			main: "subindex.js",
+		}),
+		"something-for-browser.js": "",
+		"sub/subindex.js": "",
+	},
+);
+
+createRealNodeModule(
+	"resolver-test_b",
+	{
+		main: "index.js",
+		version: "1.0.1",
+		browser: { "./foobar": "./sub", oops: false },
+	},
+	{
+		"index.js": "module.exports = {}",
+		"foobar.js": "module.exports = {}",
+		"components/MyComponent.jsx": "",
+		"sub/package.json": JSON.stringify({
+			main: "subindex.js",
+		}),
+		"something-for-browser.js": "",
+		"sub/subindex.js": "",
+	},
+);
 
 describe("Resolver test", () => {
 	describe("External modules", () => {
@@ -104,7 +141,20 @@ describe("Resolver test", () => {
 			expect(info.forcedStatement).toEqual("~/some1/index.js");
 		});
 
-		test.only("Should not Replace alias and have forcedStatement false", () => {
+		it("Should replace alias 3", () => {
+			const info = resolveModule({
+				homeDir: homeDir,
+				filePath: filePath,
+				alias: {
+					react: "resolver-test_a",
+				},
+				target: "react",
+			});
+			expect(info.package.targetAbsPath).toMatchFilePath("fuse-box/node_modules/resolver-test_a/index.js");
+			expect(info.forcedStatement).toEqual("resolver-test_a");
+		});
+
+		it("Should not Replace alias and have forcedStatement false", () => {
 			const info = resolveModule({
 				homeDir: homeDir,
 				filePath: filePath,
@@ -174,99 +224,235 @@ describe("Resolver test", () => {
 		const homeDir = path.join(cases, "src1");
 		const filePath = path.join(homeDir, "foo.js");
 		describe("From the current project", () => {
-			createRealNodeModule(
-				"resolver-a-test",
-				{
-					main: "index.js",
-					version: "1.0.1",
-				},
-				{
-					"index.js": "module.exports = {}",
-					"foobar.js": "module.exports = {}",
-					"sub/package.json": JSON.stringify({ main: "subindex.js" }),
-					"sub/subindex.js": "",
-				},
-			);
+			it("should resolve a simple package", () => {
+				const info = resolveModule({
+					homeDir: homeDir,
+					filePath: filePath,
+					target: "resolver-test_a",
+				});
 
-			// it("should resolve a simple package", () => {
-			// 	const info = resolveModule({
-			// 		homeDir: homeDir,
-			// 		filePath: filePath,
-			// 		target: "resolver-a-test",
-			// 	});
-			// 	expect(info.absPath).toContain("resolver-a-test/index.js");
-			// 	expect(info.package.name).toEqual("resolver-a-test");
-			// 	expect(info.package.version).toEqual("1.0.1");
-			// 	expect(info.fuseBoxPath).toEqual("index.js");
-			// 	expect(info.forcedStatement).toBeFalsy();
-			// });
-
-			// test.only("should resolve a simple package with partial require", () => {
-			// 	const info = resolveModule({
-			// 		homeDir: homeDir,
-			// 		filePath: filePath,
-			// 		target: "resolver-a-test/sub",
-			// 	});
-			// 	console.log(info);
-
-			// 	// this case is broken completely.. it won't give packageId.....
-			// 	// require("foo/bar")
-			// 	// should give IResolverPackage
-			// 	// packagePartial and isPackageEntry (if it's the same)
-			// });
-
-			it("should resolve a simple package with partial require with extension", () => {
-				// require("foo/bar.js")
-				// should give IResolverPackage
-				// packagePartial and isPackageEntry (if it's the same)
+				const meta = info.package.meta;
+				expect(meta.name).toEqual("resolver-test_a");
+				expect(meta.packageRoot).toMatchFilePath("fuse-box/node_modules/resolver-test_a$");
+				expect(meta.version).toEqual("1.0.1");
+				expect(meta.packageJSONLocation).toMatchFilePath("fuse-box/node_modules/resolver-test_a/package.json$");
+				expect(meta.entryFuseBoxPath).toEqual("index.js");
+				expect(info.package.isEntry).toEqual(true);
+				expect(info.package.targetAbsPath).toMatchFilePath("fuse-box/node_modules/resolver-test_a/index.js$");
+				expect(info.package.targetFuseBoxPath).toEqual("index.js");
 			});
 
-			it("should resolve a simple package with partial require on a folder", () => {
-				// require("foo/some_folder")
-				// should give IResolverPackage
-				// packagePartial and isPackageEntry (if it's the same)
+			it("should resolve a simple package with browser target", () => {
+				const info = resolveModule({
+					homeDir: homeDir,
+					filePath: filePath,
+					buildTarget: "browser",
+					target: "resolver-test_a",
+				});
+
+				const meta = info.package.meta;
+				expect(meta.entryFuseBoxPath).toEqual("something-for-browser.js");
+				expect(info.package.isEntry).toEqual(true);
+				expect(info.package.targetAbsPath).toMatchFilePath(
+					"fuse-box/node_modules/resolver-test_a/something-for-browser.js$",
+				);
+				expect(info.package.targetFuseBoxPath).toEqual("something-for-browser.js");
 			});
 
-			it("should resolve a scoped package", () => {
-				// require("@scoped/some")
-				// should give IResolverPackage
+			it("should partially resolve package", () => {
+				const info = resolveModule({
+					homeDir: homeDir,
+					filePath: filePath,
+					target: "resolver-test_a/foobar",
+				});
+
+				const meta = info.package.meta;
+				expect(meta.name).toEqual("resolver-test_a");
+				expect(meta.packageRoot).toMatchFilePath("fuse-box/node_modules/resolver-test_a$");
+				expect(meta.version).toEqual("1.0.1");
+				expect(meta.packageJSONLocation).toMatchFilePath("fuse-box/node_modules/resolver-test_a/package.json$");
+				expect(meta.entryFuseBoxPath).toEqual("index.js");
+				expect(info.package.isEntry).toEqual(false);
+				expect(info.package.targetAbsPath).toMatchFilePath("fuse-box/node_modules/resolver-test_a/foobar.js");
+				expect(info.package.targetFuseBoxPath).toEqual("foobar.js");
+				expect(info.forcedStatement).toBeFalsy();
 			});
 
-			it("should resolve a scoped package with ext", () => {
-				// require("@scoped/some/foo.js")
-				// should give IResolverPackage
+			it("should partially resolve package (with ext)", () => {
+				const info = resolveModule({
+					homeDir: homeDir,
+					filePath: filePath,
+					target: "resolver-test_a/foobar.js",
+				});
+
+				const meta = info.package.meta;
+				expect(meta.name).toEqual("resolver-test_a");
+				expect(meta.packageRoot).toMatchFilePath("fuse-box/node_modules/resolver-test_a$");
+				expect(meta.version).toEqual("1.0.1");
+				expect(meta.packageJSONLocation).toMatchFilePath("fuse-box/node_modules/resolver-test_a/package.json$");
+				expect(meta.entryFuseBoxPath).toEqual("index.js");
+				expect(info.package.isEntry).toEqual(false);
+				expect(info.package.targetAbsPath).toMatchFilePath("fuse-box/node_modules/resolver-test_a/foobar.js");
+				expect(info.package.targetFuseBoxPath).toEqual("foobar.js");
+				expect(info.forcedStatement).toBeFalsy();
+			});
+
+			it("should partially resolve package (subfile with package.json)", () => {
+				const info = resolveModule({
+					homeDir: homeDir,
+					filePath: filePath,
+					target: "resolver-test_a/sub",
+				});
+
+				const meta = info.package.meta;
+				expect(meta.name).toEqual("resolver-test_a");
+				expect(meta.packageRoot).toMatchFilePath("fuse-box/node_modules/resolver-test_a$");
+				expect(meta.version).toEqual("1.0.1");
+				expect(meta.packageJSONLocation).toMatchFilePath("fuse-box/node_modules/resolver-test_a/package.json$");
+				expect(meta.entryFuseBoxPath).toEqual("index.js");
+				expect(info.package.isEntry).toEqual(false);
+				expect(info.package.targetAbsPath).toMatchFilePath("fuse-box/node_modules/resolver-test_a/sub/subindex.js");
+				expect(info.package.targetFuseBoxPath).toEqual("sub/subindex.js");
+				expect(info.forcedStatement).toEqual("resolver-test_a/sub/subindex.js");
+			});
+
+			it("should partially resolve package (subfile with package.json) with alias", () => {
+				const info = resolveModule({
+					homeDir: homeDir,
+					filePath: filePath,
+					target: "oi/sub",
+					alias: {
+						"^oi": "resolver-test_a",
+					},
+				});
+
+				const meta = info.package.meta;
+				expect(meta.name).toEqual("resolver-test_a");
+				expect(meta.packageRoot).toMatchFilePath("fuse-box/node_modules/resolver-test_a$");
+				expect(meta.version).toEqual("1.0.1");
+				expect(meta.packageJSONLocation).toMatchFilePath("fuse-box/node_modules/resolver-test_a/package.json$");
+				expect(meta.entryFuseBoxPath).toEqual("index.js");
+				expect(info.package.isEntry).toEqual(false);
+				expect(info.package.targetAbsPath).toMatchFilePath("fuse-box/node_modules/resolver-test_a/sub/subindex.js");
+				expect(info.package.targetFuseBoxPath).toEqual("sub/subindex.js");
+				expect(info.forcedStatement).toEqual("resolver-test_a/sub/subindex.js");
 			});
 		});
 
 		describe("From existing package", () => {
+			const packageInfo = resolveModule({
+				homeDir: homeDir,
+				filePath: filePath,
+				target: "resolver-test_a",
+			});
+			const pkg = packageInfo.package;
 			it("should resolve a file being in a package", () => {
-				// const info = resolveModule({
-				// 	homeDir: homeDir,
-				// 	package: {
-				// 		name: "foo",
-				// 		main: "index.js",
-				// 		version: "1.0.0",
-				// 	},
-				// 	filePath: "/node_modules/thatfile.js",
-				// 	target: "./some5",
-				// });
+				const info = resolveModule({
+					homeDir: homeDir,
+					filePath: packageInfo.package.targetAbsPath,
+					packageMeta: pkg.meta,
+					target: "./components/MyComponent",
+				});
+
+				expect(info.extension).toEqual(".jsx");
+				expect(info.absPath).toMatchFilePath("fuse-box/node_modules/resolver-test_a/components/MyComponent.jsx");
+				expect(info.fuseBoxPath).toEqual("components/MyComponent.jsx");
+			});
+
+			it("should resolve a file being in a package 2", () => {
+				const info = resolveModule({
+					homeDir: homeDir,
+					filePath: packageInfo.package.targetAbsPath,
+					packageMeta: pkg.meta,
+					target: "./sub",
+				});
+
+				expect(info.extension).toEqual(".js");
+				expect(info.absPath).toMatchFilePath("node_modules/resolver-test_a/sub/subindex.js");
+				expect(info.fuseBoxPath).toEqual("sub/subindex.js");
+				expect(info.forcedStatement).toEqual("resolver-test_a/sub/subindex.js");
+			});
+		});
+
+		describe("resolution with browser field", () => {
+			const packageInfoB = resolveModule({
+				homeDir: homeDir,
+				filePath: filePath,
+				target: "resolver-test_b",
+			});
+			it("should resolve a file being in a package 2 (build target uknown)", () => {
+				const info = resolveModule({
+					homeDir: homeDir,
+					filePath: packageInfoB.package.targetAbsPath,
+					packageMeta: packageInfoB.package.meta,
+					target: "./foobar",
+				});
+
+				expect(info.absPath).toMatchFilePath("node_modules/resolver-test_b/foobar.js");
 			});
 
 			it("should resolve a file being in a package with browser fields", () => {
-				// const info = resolveModule({
-				// 	homeDir: homeDir,
-				// 	package: {
-				// 		name: "foo",
-				// 		main: "index.js",
-				// 		version: "1.0.0",
-				// 		browser: {
-				// 			foo: "false",
-				// 		},
-				// 	},
-				// 	filePath: "/node_modules/thatfile.js",
-				// 	target: "./foo",
-				// });
-				// should give "fuse-empty-package" as an alias here
+				const info = resolveModule({
+					homeDir: homeDir,
+					filePath: packageInfoB.package.targetAbsPath,
+					packageMeta: packageInfoB.package.meta,
+					target: "./foobar",
+					buildTarget: "browser",
+				});
+
+				expect(info.absPath).toMatchFilePath("node_modules/resolver-test_b/sub/subindex.js$");
+				expect(info.forcedStatement).toEqual("resolver-test_b/sub/subindex.js");
+				expect(info.fuseBoxPath).toEqual("sub/subindex.js");
+			});
+
+			it("should replace the file with an empty package", () => {
+				const info = resolveModule({
+					homeDir: homeDir,
+					filePath: packageInfoB.package.targetAbsPath,
+					packageMeta: packageInfoB.package.meta,
+					target: "oops",
+					modules: [customModules],
+					buildTarget: "browser",
+				});
+
+				expect(info.forcedStatement).toEqual("fuse-empty-package");
+				expect(info.package.meta.name).toEqual("fuse-empty-package");
+				expect(info.package.targetAbsPath).toMatchFilePath("cases/_modules/fuse-empty-package/index.js$");
+			});
+		});
+
+		describe("Server/electron polyfill should not be bundled", () => {
+			const filePath = path.join(homeDir, "foo.js");
+			it("Should skip fs (server)", () => {
+				const info = resolveModule({
+					homeDir: homeDir,
+					filePath: filePath,
+					target: "fs",
+					buildTarget: "server",
+				});
+				expect(info.skip).toEqual(true);
+			});
+
+			it("Should skip fs (electron)", () => {
+				const info = resolveModule({
+					homeDir: homeDir,
+					filePath: filePath,
+					target: "fs",
+					buildTarget: "electron",
+				});
+				expect(info.skip).toEqual(true);
+			});
+
+			it("Should not skip fs (browser)", () => {
+				const info = resolveModule({
+					homeDir: homeDir,
+					filePath: filePath,
+					target: "fs",
+					buildTarget: "browser",
+					modules: [customModules],
+				});
+				expect(info.skip).toBeFalsy();
+				expect(info.package.targetAbsPath).toMatchFilePath("cases/_modules/fs/index.js$");
 			});
 		});
 	});
--- src/_v4/transform/_playground.ts
+++ src/_v4/transform/_playground.ts
@@ -1,10 +1,25 @@
 import { fastTransform } from "./fastTransform";
+import * as ts from "typescript";
 const name = 1;
 
 const str = `
 require('foo')
 `;
 
-const code = fastTransform({ input: str });
+const tsString = `
+import * as a from "a";
+const shit = 1;
+export class Some<T> {
+	constructor() {}
+	public start() : boolean{
+		return true;
+	}
+}
 
-console.log(code);
+`;
+
+const data = ts.createSourceFile("oi.ts", tsString, ts.ScriptTarget.ESNext);
+console.log(data);
+// const code = fastTransform({ input: str });
+
+// console.log(code);
