838cd28290db3ca440e0d04ea0d910cb40454d5a
Unknown <nchanged@gmail.com>
2019-03-15 16:31:42 +0100

    feat: new typescript plugin

--- src/plugins/core/plugin_typescript.ts
+++ src/plugins/core/plugin_typescript.ts
@@ -1,21 +1,42 @@
 import { Context } from '../../core/Context';
 import { Module } from '../../core/Module';
-import { fastTransform } from '../../transform/fastTransform';
 import { tsTransform } from '../../transform/tsTransform';
+import * as ts from 'typescript';
 
 export function pluginTypescript() {
   return (ctx: Context) => {
     const ict = ctx.interceptor;
+
     ict.on('bundle_resolve_module', (props: { module: Module }) => {
       const module = props.module;
+      const pkg = module.pkg;
+      const config = ctx.config;
       const analysis = module.fastAnalysis;
-      if ((!analysis && !analysis.report) || analysis.report.transpiled || !module.contents) {
+      if (!analysis || !analysis.report || analysis.report.transpiled || !module.contents) {
         return props;
       }
+
+      let requireSourceMaps = true;
+
+      if (pkg.isDefaultPackage) {
+        if (!config.options.projectSourceMap) {
+          requireSourceMaps = false;
+        }
+      } else {
+        if (!config.options.vendorSourceMap) {
+          requireSourceMaps = false;
+        }
+      }
+
+      const compilerOptions: ts.CompilerOptions = {
+        ...ctx.tsConfig.compilerOptions,
+        sourceMap: requireSourceMaps,
+      };
 
       ict.promise(async () => {
         const data = tsTransform({
           input: module.contents,
+          compilerOptions: compilerOptions,
           replacements: !analysis.report.statementsReplaced && analysis.replaceable,
         });
         module.contents = data.outputText;
