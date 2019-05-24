030b7d075e0db4790c1bc01a11254a6863098285
Gustorn <gustorn@gmail.com>
2019-05-24 06:00:01 +0200

    fix: Synthetic default export polyfill does not add __esModule property

--- modules/fuse-box-responsive-api/index.js
+++ modules/fuse-box-responsive-api/index.js
@@ -9,26 +9,30 @@
 	/* @if allowSyntheticDefaultImports */
 	// NOTE: Should match syntheticDefaultExportPolyfill in LoaderAPI.ts
 	function syntheticDefaultExportPolyfill(input) {
-		if (
-			input === null ||
-			["function", "object", "array"].indexOf(typeof input) === -1 ||
-			input.hasOwnProperty("default") // use hasOwnProperty to avoid triggering usage warnings from libraries like mobx
-		) {
+		if (input === null || ['function', 'object', 'array'].indexOf(typeof input) === -1) {
 			return;
 		}
 
-		// to get around frozen input
+		var hasDefaultProperty = input.hasOwnProperty('default');
+		var hasModuleProperty = input.hasOwnProperty('__esModule');
 		if (Object.isFrozen(input)) {
-			input.default = input;
+			if (!hasDefaultProperty) {
+				input['default'] = input;
+			}
+
+			if (!hasModuleProperty) {
+				input['__esModule'] = true;
+			}
 			return;
 		}
 
-		// free to define properties
-		Object.defineProperty(input, "default", {
-			value: input,
-			writable: true,
-			enumerable: false
-		});
+		if (!hasDefaultProperty) {
+			Object.defineProperty(input, 'default', { value: input, writable: true, enumerable: false });
+		}
+
+		if (!hasModuleProperty) {
+			Object.defineProperty(input, '__esModule', { value: true });
+		}
 	}
 	/* @end */
 
