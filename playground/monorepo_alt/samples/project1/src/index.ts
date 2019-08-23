b35478eef5d135f2af01b7b6b3bc5500467b6e4b
Vegar RIngdal <vegar.ringdal@gmail.com>
2019-08-23 19:05:15 +0200

    chore: add playground sample - monorepo alt

---
+++ playground/monorepo_alt/samples/project1/src/index.ts
@@ -0,0 +1,6 @@
+import lib1 from '@myorg/core';
+import {kernel} from '@myorg/kernel';
+console.log(lib1, kernel);
+
+// just to see it works..
+var project1:number = "project1_error";
---
+++ playground/monorepo_alt/samples/project2/src/index.ts
@@ -0,0 +1,6 @@
+import lib1 from '@myorg/core';
+import {kernel} from '@myorg/kernel';
+console.log(lib1, kernel);
+
+// just to see it works..
+var project2:number = "project2_error";
