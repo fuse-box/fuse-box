a05f01b58b11209bd2b77ed84094b7ee6f9d036d
kyr0 <info@aron-homberg.de>
2019-09-01 14:20:14 +0200

    feat(pluginCustomTransform): Improved playground example

--- playground/custom_transform/fuse.ts
+++ playground/custom_transform/fuse.ts
@@ -3,10 +3,10 @@ import { fusebox, pluginCustomTransform } from '../../src';
 
 const isProd = process.argv.indexOf('--prod') > -1;
 
-function someCustomTransformer<T extends ts.Node>(): ts.TransformerFactory<T> {
+function customTransformer<T extends ts.Node>(type: string): ts.TransformerFactory<T> {
   return context => {
     console.log();
-    console.log('===> someCustomTransformer called');
+    console.log(`===> customTransformer ${type} called`);
     console.log();
 
     const visit: ts.Visitor = node => {
@@ -32,9 +32,8 @@ const fuse = fusebox({
   cache: false,
   plugins: [
     pluginCustomTransform({
-      before: [someCustomTransformer()],
-      after: [someCustomTransformer()],
-      afterDeclarations: [someCustomTransformer()],
+      before: [customTransformer('before')],
+      after: [customTransformer('after')],
     }),
   ],
 });
