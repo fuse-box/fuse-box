4add9f1eb8d40e7c767288d8d7138d80c4ff94e3
kyr0 <info@aron-homberg.de>
2019-09-07 00:36:39 +0200

    feat(AwaitableInterceptor): Added the ability to async/await

--- src/interceptor/interceptor.ts
+++ src/interceptor/interceptor.ts
@@ -4,8 +4,10 @@ interface TypedInterceptor<T> {
   getPromises: () => Array<any>;
   promise: (fn: () => Promise<any>) => void;
   resolve: () => Promise<any>;
-  on<K extends keyof T>(s: K, props: (props: T[K]) => T[K]);
-  sync<K extends keyof T>(s: K, props: T[K]): T[K];
+  on<K extends keyof T>(key: K, fn: (props: T[K]) => T[K]);
+  sync<K extends keyof T>(key: K, props: T[K]): T[K];
+  awaitForSync<K extends keyof T>(key: K, props: T[K]): Promise<T[K]>;
+  awaitOn<K extends keyof T>(key: K, fn: (props: T[K]) => Promise<T[K]>);
 }
 export type MainInterceptor = TypedInterceptor<InterceptorEvents>;
 export function createInterceptor(): MainInterceptor {
@@ -21,6 +23,11 @@ export function createInterceptor(): MainInterceptor {
     }
     fns.push(fn);
   }
+
+  const on = function(key: any, fn: (props: any) => any) {
+    add(key, fn);
+  };
+
   return {
     getPromises: () => promises,
     promise: function(fn: () => Promise<any>) {
@@ -31,14 +38,29 @@ export function createInterceptor(): MainInterceptor {
       promises = [];
       return res;
     },
-    on: function(key: any, fn: (props: any) => any) {
-      add(key, fn);
-    },
+    on,
     // sync (emit an even which should return an according props
     sync: function(key: string, props: any) {
       if (subscriptions.has(key)) {
         const fns = subscriptions.get(key);
         const responses = fns.map(fn => fn(props));
+        if (responses.length > 0) {
+          // return the latest response
+          return responses[responses.length - 1];
+        }
+      }
+      return props;
+    },
+    awaitOn: on,
+    awaitForSync: async function(key: string, props: any) {
+      if (subscriptions.has(key)) {
+        const fns = subscriptions.get(key);
+        const responses = [];
+
+        for (let i = 0; i < fns.length; i++) {
+          responses.push(await fns[i](props));
+        }
+
         if (responses.length > 0) {
           // return the latest response
           return responses[responses.length - 1];
