ace5b3bfb5e316545bb5b8ad6bac222d981da78f
Vegar RIngdal <vegar.ringdal@gmail.com>
2019-09-07 22:11:02 +0200

    chore: Precedence test

--- src/resolver/__tests__/shared.test.ts
+++ src/resolver/__tests__/shared.test.ts
@@ -31,5 +31,34 @@ describe('shared functions', () => {
       const res = getFolderEntryPointFromPackageJSON({ json: {} });
       expect(res).toEqual('index.js');
     });
+
+    it('Precedence test - all types & useLocalField: true - should return "local:main"', () => {
+      const res = getFolderEntryPointFromPackageJSON({
+        useLocalField: true,
+        json: { main: 'main.js', 'ts:main': 'tsmain.ts', 'local:main': 'localmain.js', module: 'module.js' },
+      });
+      expect(res).toEqual('localmain.js');
+    });
+
+    it('Precedence test - all types & useLocalField: false - should return "ts:main"', () => {
+      const res = getFolderEntryPointFromPackageJSON({
+        json: { main: 'main.js', 'ts:main': 'tsmain.ts', 'local:main': 'localmain.js', module: 'module.js' },
+      });
+      expect(res).toEqual('tsmain.ts');
+    });
+
+    it('Precedence test - missing ts:main - should return "module"', () => {
+      const res = getFolderEntryPointFromPackageJSON({
+        json: { main: 'main.js', 'local:main': 'localmain.js', module: 'module.js' },
+      });
+      expect(res).toEqual('module.js');
+    });
+
+    it('Precedence test - missing ts:main or module - should return "main"', () => {
+      const res = getFolderEntryPointFromPackageJSON({
+        json: { main: 'main.js', 'local:main': 'localmain.js' },
+      });
+      expect(res).toEqual('main.js');
+    });
   });
 });
