e57591bb191482ad3728abbc35571b036f5c6b9c
Vegar RIngdal <vegar.ringdal@gmail.com>
2019-09-07 17:57:29 +0200

    chore: review comment fixes + add quickfix to type errors

--- src/plugins/core/plugin_minify_html_literals.ts
+++ src/plugins/core/plugin_minify_html_literals.ts
@@ -1,12 +1,31 @@
-import { minifyHTMLLiterals } from 'minify-html-literals';
 import { Context } from '../../core/Context';
 
-export function pluginMinifyHtmlLiterals(...options: any) {
+/**
+ * See https://github.com/asyncLiz/minify-html-literals after source input
+ */
+interface IPluginMinifyHtmlLiterals {}
+
+/**
+ * Simple plugin to use npm module minify-html-literals
+ * @param options
+ */
+export function pluginMinifyHtmlLiterals(...options: IPluginMinifyHtmlLiterals[]) {
   const useroptions = options;
+
   return (ctx: Context) => {
-    ctx.ict.on('assemble_fast_analysis', props => {
+    ctx.ict.on('bundle_resolve_module', props => {
       const { module } = props;
-      if (module.props && (module.props.extension === '.ts' || module.props.extension === '.js')) {
+      if (module.isExecutable()) {
+        // verify if module is installed
+        if (!ctx.isInstalled('minify-html-literals')) {
+          ctx.fatal(`Fatal error when capturing ${module.props.absPath}`, [
+            'Module "minify-html-literals" is required, Please install it using the following command',
+            'npm install minify-html-literals --save-dev',
+          ]);
+          return;
+        }
+
+        // check if we have any html`` or css`` template tags
         if (module.contents.indexOf('html`') !== -1 || module.contents.indexOf('css`') !== -1) {
           ctx.log.info('pluginMinifyHtmlLiterals', 'minifying $file', {
             file: module.props.absPath,
@@ -15,6 +34,7 @@ export function pluginMinifyHtmlLiterals(...options: any) {
           // https://github.com/asyncLiz/minify-html-literals
           let newcontent;
           try {
+            const minifyHTMLLiterals = require('minify-html-literals');
             newcontent = minifyHTMLLiterals(module.contents, ...useroptions);
           } catch {}
 
