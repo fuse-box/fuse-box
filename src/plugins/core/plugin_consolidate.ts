738ede1193216eece9265f7c971a05bd551c5132
Unknown <nchanged@gmail.com>
2019-09-09 11:10:15 +0300

    chore: typings issue

--- src/plugins/core/plugin_consolidate.ts
+++ src/plugins/core/plugin_consolidate.ts
@@ -1,27 +1,23 @@
-import { Context } from '../../core/Context';
-import { IBundleWriteResponse } from '../../bundle/Bundle';
 import * as consolidate from 'consolidate';
+import { Context } from '../../core/Context';
 
 export function pluginConsolidate(engine: string, options: any) {
   return async (ctx: Context) => {
-    ctx.ict.waitFor(
-      'before_webindex_write',
-      async (props: { filePath: string; fileContents: string; bundles: Array<IBundleWriteResponse> }) => {
-        if (typeof consolidate[engine] !== 'function') {
-          ctx.fatal(`The template engine you selected is not available in consolidate: ${engine}`);
-        }
+    ctx.ict.waitFor('before_webindex_write', async props => {
+      if (typeof consolidate[engine] !== 'function') {
+        ctx.fatal(`The template engine you selected is not available in consolidate: ${engine}`);
+      }
 
-        try {
-          const processedTemplate = await consolidate[engine](props.filePath, {
-            ...options,
-            bundles: props.bundles,
-          });
-          props.fileContents = processedTemplate;
-        } catch (e) {
-          ctx.fatal('Error processing the web-index template using consolidate.', [e.message]);
-        }
-        return props;
-      },
-    );
+      try {
+        const processedTemplate = await consolidate[engine](props.filePath, {
+          ...options,
+          bundles: props.bundles,
+        });
+        props.fileContents = processedTemplate;
+      } catch (e) {
+        ctx.fatal('Error processing the web-index template using consolidate.', [e.message]);
+      }
+      return props;
+    });
   };
 }
