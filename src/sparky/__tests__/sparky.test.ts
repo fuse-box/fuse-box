db2c12ea348d5f2d2260a033cdbd86d2e9dbb365
Vegar RIngdal <vegar.ringdal@gmail.com>
2019-09-11 23:27:40 +0200

    test: sparky copy/bump test by changing ENV.SCRIPT_PATH

--- src/sparky/__tests__/sparky.test.ts
+++ src/sparky/__tests__/sparky.test.ts
@@ -1,5 +1,7 @@
 import '../../utils/test_utils';
 import { sparky } from '../sparky';
+import { env } from '../../env';
+import * as path from 'path';
 
 function mockSparkySrc(glob: string) {
   class SparkyContext {}
@@ -76,6 +78,146 @@ describe('Sparky test', () => {
       .exec();
     const readFiles = chain['__scope']().readFiles;
     expect(readFiles[Object.keys(readFiles)[0]]).toEqual('some text');
+  });
+
+  it('sparky script path check 1 - base:"__test__/case1/"', async () => {
+    //
+    // override ENV so we simulate script staretd somewhere else
+    // set to subfolder './case1
+    env.SCRIPT_PATH = path.normalize(`${__dirname}/case1/`);
+
+    class SparkyContext {}
+    const { src, rm } = sparky<SparkyContext>(SparkyContext);
+
+    // remove old if any
+    rm('./dist');
+    await src(`../stubs/**/**.*`)
+      .dest('dist', 'stubs')
+      .exec();
+
+    // check if it have file
+    const chain = mockSparkySrc(`dist/case-a/**/**.*`);
+    const list = await chain
+      .filter(/.*\.(txt|md)$/) // check if filter works
+      .filter(/foo\.md$/)
+      .exec();
+    expect(list).toHaveLength(1);
+
+    // cleanup and test
+    rm('dist');
+    const chain2 = mockSparkySrc(`dist/case-a/**/**.*`);
+    const list2 = await chain2
+      .filter(/.*\.(txt|md)$/)
+      .filter(/foo\.md$/)
+      .exec();
+    expect(list2).toHaveLength(0);
+  });
+
+  it('sparky script path check 2 - base:"__test__/"', async () => {
+    // override ENV so we simulate script staretd somewhere else
+    // use current script path
+    env.SCRIPT_PATH = path.normalize(`${__dirname}`);
+
+    class SparkyContext {}
+    const { src, rm } = sparky<SparkyContext>(SparkyContext);
+
+    // remove old if any
+    rm('./case1/dist');
+    await src(`./stubs/**/**.*`) // check if filter works
+      .dest('case1/dist', 'stubs')
+      .exec();
+
+    const chain = mockSparkySrc(`./case1/dist/case-a/**/**.*`);
+    const list = await chain
+      .filter(/.*\.(txt|md)$/)
+      .filter(/foo\.md$/)
+      .exec();
+    expect(list).toHaveLength(1);
+
+    // cleanup and test
+    rm('./case1/dist');
+    const chain2 = mockSparkySrc(`./case1/dist/case-a/**/**.*`);
+    const list2 = await chain2
+      .filter(/.*\.(txt|md)$/)
+      .filter(/foo\.md$/)
+      .exec();
+    expect(list2).toHaveLength(0);
+  });
+
+  it('sparky script path check 3 - base:"__test__/" with parent path on checks', async () => {
+    // override ENV so we simulate script staretd somewhere else
+    // use current script file path
+    env.SCRIPT_PATH = path.normalize(`${__dirname}`);
+
+    class SparkyContext {}
+    const { src, rm } = sparky<SparkyContext>(SparkyContext);
+
+    // remove old if any
+    rm('./case1/dist');
+    await src(`./stubs/**/**.*`) // check if filter works
+      .dest('case1/dist', 'stubs')
+      .exec();
+
+    // go crazy and change paths of script again tp parent folder
+    // can user also do this ?
+    env.SCRIPT_PATH = path.normalize(path.resolve(__dirname, '../'));
+
+    const chain = mockSparkySrc(`__tests__/case1/dist/case-a/**/**.*`);
+    const list = await chain
+      .filter(/.*\.(txt|md)$/)
+      .filter(/foo\.md$/)
+      .exec();
+    expect(list).toHaveLength(1);
+
+    // cleanup and test
+    rm('__tests__/case1/dist');
+    const chain2 = mockSparkySrc(`__tests__/case1/dist/case-a/**/**.*`);
+    const list2 = await chain2
+      .filter(/.*\.(txt|md)$/)
+      .filter(/foo\.md$/)
+      .exec();
+    expect(list2).toHaveLength(0);
+  });
+
+  it('sparky script path check 3 - bump check', async () => {
+    // override ENV so we simulate script staretd somewhere else
+    // use ./case1
+    env.SCRIPT_PATH = path.normalize(`${__dirname}/case1`);
+
+    class SparkyContext {}
+    const { src, rm } = sparky<SparkyContext>(SparkyContext);
+
+    // remove old if any
+    rm('dist');
+
+    // make copy, so we dont modify original
+    await src('pack.json')
+      .dest('dist/', env.SCRIPT_PATH)
+      .exec();
+
+    // bump and make copy like build does
+    await src('dist/pack.json')
+      .bumpVersion('dist/pack.json', { type: 'next' })
+      .write()
+      .dest('dist/modified', 'dist')
+      .exec();
+
+    // get both files and check version
+    const chain = mockSparkySrc(`dist/**/**.*`);
+    const list = await chain
+      .contentsOf(/pack\.json$/, str => {
+        const json = JSON.parse(str);
+        expect(json.version).toEqual('4.0.0-next.137');
+        return json;
+      })
+      .exec();
+    expect(list).toHaveLength(2);
+
+    // cleanup and test
+    rm('dist');
+    const chain2 = mockSparkySrc(`dist/**/**.*`);
+    const list2 = await chain2.filter(/pack\.json$/).exec();
+    expect(list2).toHaveLength(0);
   });
 
   // it('should filter with function false', async () => {
