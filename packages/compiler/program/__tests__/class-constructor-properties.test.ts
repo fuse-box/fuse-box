39e3af67139db7afc1a88dee0a7ac79557293444
kyr0 <info@aron-homberg.de>
2019-09-29 00:09:48 +0200

    feat(classConstructorProps): Optimized impl. and added complex tests

--- packages/compiler/program/__tests__/class-constructor-properties.test.ts
+++ packages/compiler/program/__tests__/class-constructor-properties.test.ts
@@ -1,7 +1,7 @@
-import { compileModule } from "../compileModule";
+import { compileModule } from '../compileModule';
 
-describe("Class constructor properties", () => {
-  it("should initialize constructor properties in the constructor", () => {
+describe('Class constructor properties', () => {
+  it('should initialize constructor properties in the constructor', () => {
     const result = compileModule({
       code: `
 
@@ -16,7 +16,7 @@ describe("Class constructor properties", () => {
                     this.fuse = fuse;
                 } 
             }
-        `
+        `,
     });
 
     expect(result.code).toEqual(`class HelloWorld {
@@ -30,7 +30,7 @@ describe("Class constructor properties", () => {
 `);
   });
 
-  it("should work with super classes", () => {
+  it('should work with super classes', () => {
     const result = compileModule({
       code: `
             class Amazing {}
@@ -48,7 +48,7 @@ describe("Class constructor properties", () => {
                     this.fuse = fuse;
                 } 
             }
-        `
+        `,
     });
 
     expect(result.code).toEqual(`class Amazing {}
@@ -59,6 +59,246 @@ class HelloWorld extends Amazing {
     this.to = to;
     this.awesomeness = awesomeness;
     this.fuse = fuse;
+  }
+}
+`);
+  });
+
+  it('should not add initializer calls in standard block statements', () => {
+    const result = compileModule({
+      code: `
+            class Amazing {}
+
+            class HelloWorld extends Amazing {
+
+                constructor(public welcome: string, 
+                            private to: string, 
+                            protected awesomeness: string,
+                            of: number,
+                            fuse?: boolean) {
+                                
+                    super();
+
+                    (() => {console.log('Freaky block statement here.');})();
+
+                    this.fuse = fuse;
+                }
+
+                welcomeToTheBlock() {
+                  
+                }
+            }
+        `,
+    });
+
+    expect(result.code).toEqual(`class Amazing {}
+class HelloWorld extends Amazing {
+  constructor(welcome, to, awesomeness, of, fuse) {
+    super();
+    this.welcome = welcome;
+    this.to = to;
+    this.awesomeness = awesomeness;
+    (() => {
+      console.log("Freaky block statement here.");
+    })();
+    this.fuse = fuse;
+  }
+  welcomeToTheBlock() {}
+}
+`);
+  });
+
+  it('should ignore decorators for now', () => {
+    const result = compileModule({
+      code: `
+            class Amazing {}
+
+            class HelloWorld extends Amazing {
+
+                constructor(@foo public welcome: string, 
+                            @foo() private to: string, 
+                            @foo(123, 'ab') protected awesomeness: string,
+                            of: number,
+                            fuse?: boolean) {
+                                
+                    super();
+
+                    (() => {console.log('Freaky block statement here.');})();
+
+                    this.fuse = fuse;
+                }
+
+                welcomeToTheBlock() {
+                  
+                }
+            }
+        `,
+    });
+
+    expect(result.code).toEqual(`class Amazing {}
+class HelloWorld extends Amazing {
+  constructor(welcome, to, awesomeness, of, fuse) {
+    super();
+    this.welcome = welcome;
+    this.to = to;
+    this.awesomeness = awesomeness;
+    (() => {
+      console.log("Freaky block statement here.");
+    })();
+    this.fuse = fuse;
+  }
+  welcomeToTheBlock() {}
+}
+`);
+  });
+
+  it('should leave classes without constructor props alone', () => {
+    const result = compileModule({
+      code: `
+
+            class HelloWorld {
+
+                constructor(welcome: string, 
+                            to: string, 
+                            awesomeness: string,
+                            of: number,
+                            fuse?: boolean) {
+                                
+                    this.fuse = fuse;
+                } 
+            }
+        `,
+    });
+
+    expect(result.code).toEqual(`class HelloWorld {
+  constructor(welcome, to, awesomeness, of, fuse) {
+    this.fuse = fuse;
+  }
+}
+`);
+  });
+
+  it('should deal with inner classes', () => {
+    const result = compileModule({
+      code: `
+            class Amazing {}
+
+            class HelloWorld extends Amazing {
+
+                constructor(@foo public welcome: string, 
+                            @foo() private to: string, 
+                            @foo(123, 'ab') protected awesomeness: string,
+                            of: number,
+                            fuse?: boolean) {
+                                
+                    super();
+
+                    const innerClass = class extends Amazing {
+
+                      constructor(@foo public welcome: string, 
+                        @foo() private to: string, 
+                        @foo(123, 'ab') protected awesomeness: string,
+                        of: number,
+                        fuse?: boolean) {
+                          super();
+                        }
+                    }
+
+                    this.fuse = fuse;
+                }
+
+                welcomeToTheBlock() {
+                  
+                }
+            }
+        `,
+    });
+
+    expect(result.code).toEqual(`class Amazing {}
+class HelloWorld extends Amazing {
+  constructor(welcome, to, awesomeness, of, fuse) {
+    super();
+    this.welcome = welcome;
+    this.to = to;
+    this.awesomeness = awesomeness;
+    const innerClass = class extends Amazing {
+      constructor(welcome, to, awesomeness, of, fuse) {
+        super();
+        this.welcome = welcome;
+        this.to = to;
+        this.awesomeness = awesomeness;
+      }
+    };
+    this.fuse = fuse;
+  }
+  welcomeToTheBlock() {}
+}
+`);
+  });
+
+  it('should deal with immediate function calls on initialized properties', () => {
+    const result = compileModule({
+      code: `
+            
+            class HelloWorld {
+
+                constructor(public welcome : string) {
+                  console.log(this.welcome);
+                }
+            }
+        `,
+    });
+
+    expect(result.code).toEqual(`class HelloWorld {
+  constructor(welcome) {
+    this.welcome = welcome;
+    console.log(this.welcome);
+  }
+}
+`);
+  });
+
+  it('should deal with multiple class definitions following each other', () => {
+    const result = compileModule({
+      code: `
+            
+            class HelloWorld {
+
+                constructor(public welcome : string) {
+                  console.log(this.welcome);
+                }
+            }
+            class HelloWorld2 {
+
+              constructor(public welcome2 : string) {
+                console.log(this.welcome2);
+              }
+          }
+          class HelloWorld3 {
+
+            constructor(public welcome3 : string) {
+              console.log(this.welcome3);
+            }
+        }
+        `,
+    });
+
+    expect(result.code).toEqual(`class HelloWorld {
+  constructor(welcome) {
+    this.welcome = welcome;
+    console.log(this.welcome);
+  }
+}
+class HelloWorld2 {
+  constructor(welcome2) {
+    this.welcome2 = welcome2;
+    console.log(this.welcome2);
+  }
+}
+class HelloWorld3 {
+  constructor(welcome3) {
+    this.welcome3 = welcome3;
+    console.log(this.welcome3);
   }
 }
 `);
--- packages/compiler/transformers/ClassConstructorPropertyTransformer.ts
+++ packages/compiler/transformers/ClassConstructorPropertyTransformer.ts
@@ -1,51 +1,65 @@
-import { ASTNode } from "../interfaces/AST";
-import { ITransformer } from "../program/transpileModule";
-import { IVisit } from "../Visitor/Visitor";
+import { ASTNode } from '../interfaces/AST';
+import { ITransformer } from '../program/transpileModule';
+import { IVisit } from '../Visitor/Visitor';
+
+let paramtersFound: Array<string> = [];
 
 export function ClassConstructorPropertyTransformer(): ITransformer {
   return (visit: IVisit) => {
     const node = visit.node;
 
     switch (node.type) {
-      case "ParameterProperty":
-        if (node.accessibility) {
-          return {
-            replaceWith: { type: "Identifier", name: node.parameter.name }
-          };
-        }
+      case 'ParameterProperty':
+        // filter for parameters with an accessibility modifier (private, protected, public)
+        if (!node.accessibility) return;
 
-      case "BlockStatement":
-        const body = node.body as Array<ASTNode>;
-        const firstCallExpression = body[0];
+        // remember it for fast iteration in constructor BlockStatement
+        paramtersFound.push(node.parameter.name);
+        return {
+          replaceWith: { type: 'Identifier', name: node.parameter.name },
+        };
+
+      case 'BlockStatement':
+        // value > 0 makes sure it is the first block statement (constructor block)
+        // and that there is AST mutation work to do (add initialization assignments)
+        if (!paramtersFound.length) return;
+
+        const firstCallExpression = node.body[0];
+
+        // usually add assignments at first, except there is a super() call
         let insertPosition = 0;
 
         if (
           firstCallExpression &&
           firstCallExpression.expression &&
-          firstCallExpression.expression.callee
+          firstCallExpression.expression.callee &&
+          firstCallExpression.expression.callee.type === 'Super'
         ) {
+          // start injecting initializations at pos. 1 because super() call
+          // must always be the first call
           insertPosition = 1;
         }
 
-        for (let param of visit.parent.params) {
-          if (param.accessibility) {
-            body.splice(insertPosition, 0, {
-              type: "ExpressionStatement",
-              expression: {
-                type: "AssignmentExpression",
-                left: {
-                  type: "MemberExpression",
-                  object: { type: "ThisExpression" },
-                  computed: false,
-                  property: { type: "Identifier", name: param.parameter.name }
-                },
-                operator: "=",
-                right: { type: "Identifier", name: param.parameter.name }
-              }
-            });
-            insertPosition++;
-          }
+        for (let parameterName of paramtersFound) {
+          (<Array<ASTNode>>node.body).splice(insertPosition, 0, {
+            type: 'ExpressionStatement',
+            expression: {
+              type: 'AssignmentExpression',
+              left: {
+                type: 'MemberExpression',
+                object: { type: 'ThisExpression' },
+                computed: false,
+                property: { type: 'Identifier', name: parameterName },
+              },
+              operator: '=',
+              right: { type: 'Identifier', name: parameterName },
+            },
+          });
+          insertPosition++;
         }
+        // reset the state so transformation doesn't happen
+        // in other BlockStatements
+        paramtersFound = [];
     }
   };
 }
