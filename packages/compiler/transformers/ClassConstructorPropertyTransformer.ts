6fcc07e30e3e89f9e27426ee77979277d82985c5
Unknown <nchanged@gmail.com>
2019-09-29 15:18:25 +0200

    fix: optimise constructor props

--- packages/compiler/transformers/ClassConstructorPropertyTransformer.ts
+++ packages/compiler/transformers/ClassConstructorPropertyTransformer.ts
@@ -2,64 +2,59 @@ import { ASTNode } from '../interfaces/AST';
 import { ITransformer } from '../program/transpileModule';
 import { IVisit } from '../Visitor/Visitor';
 
-let paramtersFound: Array<string> = [];
-
 export function ClassConstructorPropertyTransformer(): ITransformer {
   return (visit: IVisit) => {
-    const node = visit.node;
+    const { node } = visit;
+    //console.log(scope, node);
 
-    switch (node.type) {
-      case 'ParameterProperty':
-        // filter for parameters with an accessibility modifier (private, protected, public)
-        if (!node.accessibility) return;
-
-        // remember it for fast iteration in constructor BlockStatement
-        paramtersFound.push(node.parameter.name);
-        return {
-          replaceWith: { type: 'Identifier', name: node.parameter.name },
-        };
-
-      case 'BlockStatement':
-        // value > 0 makes sure it is the first block statement (constructor block)
-        // and that there is AST mutation work to do (add initialization assignments)
-        if (!paramtersFound.length) return;
-
-        const firstCallExpression = node.body[0];
-
-        // usually add assignments at first, except there is a super() call
-        let insertPosition = 0;
-
-        if (
-          firstCallExpression &&
-          firstCallExpression.expression &&
-          firstCallExpression.expression.callee &&
-          firstCallExpression.expression.callee.type === 'Super'
-        ) {
-          // start injecting initializations at pos. 1 because super() call
-          // must always be the first call
-          insertPosition = 1;
+    if (node.type === 'MethodDefinition' && node.kind === 'constructor') {
+      if (node.value.params) {
+        let index = 0;
+        let hasSomethingToAdd = false;
+        let thisParams = [];
+        while (index < node.value.params.length) {
+          const param = node.value.params[index] as ASTNode;
+          if (param.type === 'ParameterProperty' && param.accessibility) {
+            hasSomethingToAdd = true;
+            thisParams.push(param.parameter.name);
+            node.value.params[index] = { type: 'Identifier', name: param.parameter.name };
+          }
+          index++;
         }
 
-        for (let parameterName of paramtersFound) {
-          (<Array<ASTNode>>node.body).splice(insertPosition, 0, {
-            type: 'ExpressionStatement',
-            expression: {
-              type: 'AssignmentExpression',
-              left: {
-                type: 'MemberExpression',
-                object: { type: 'ThisExpression' },
-                computed: false,
-                property: { type: 'Identifier', name: parameterName },
+        if (hasSomethingToAdd) {
+          const body = node.value.body.body;
+          const firstCallExpression = body[0];
+          let insertPosition = 0;
+          if (
+            firstCallExpression &&
+            firstCallExpression.expression &&
+            firstCallExpression.expression.callee &&
+            firstCallExpression.expression.callee.type === 'Super'
+          ) {
+            // start injecting initializations at pos. 1 because super() call
+            // must always be the first call
+            insertPosition = 1;
+          }
+          for (const paramName of thisParams) {
+            (<Array<ASTNode>>body).splice(insertPosition, 0, {
+              type: 'ExpressionStatement',
+              expression: {
+                type: 'AssignmentExpression',
+                left: {
+                  type: 'MemberExpression',
+                  object: { type: 'ThisExpression' },
+                  computed: false,
+                  property: { type: 'Identifier', name: paramName },
+                },
+                operator: '=',
+                right: { type: 'Identifier', name: paramName },
               },
-              operator: '=',
-              right: { type: 'Identifier', name: parameterName },
-            },
-          });
-          insertPosition++;
+            });
+            insertPosition++;
+          }
         }
-        // reset the state so transformation doesn't happen
-        // in other BlockStatements
-        paramtersFound = [];
+      }
     }
   };
 }
