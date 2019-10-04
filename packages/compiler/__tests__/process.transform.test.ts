e581f515d7f491e01b216c484718c7d7013ab4b4
Unknown <nchanged@gmail.com>
2019-10-04 15:36:26 +0200

    feat(compiler): process handle

--- packages/compiler/__tests__/process.transform.test.ts
+++ packages/compiler/__tests__/process.transform.test.ts
@@ -1,10 +1,94 @@
 import { compileModule } from '../program/compileModule';
 describe('Process transform test', () => {
-  it('should transform process env', () => {
-    const result = compileModule({
-      code: `
-        console.log(process.env.NODE_ENV);
-    `,
+  describe('process.env.***', () => {
+    it.only('should replace process.env.NODE_ENV', () => {
+      const result = compileModule({
+        bundleProps: { target: 'browser', env: { NODE_ENV: 'development' } },
+        code: `
+          console.log(process.env.NODE_ENV);
+      `,
+      });
+      expect(result.code).toMatchSnapshot();
+    });
+  });
+  describe('process.xxx', () => {
+    it('should transform process.version', () => {
+      const result = compileModule({
+        code: `
+          console.log(process.version);
+      `,
+      });
+      expect(result.code).toMatchSnapshot();
+    });
+
+    it('should transform process.versions', () => {
+      const result = compileModule({
+        code: `console.log(process.versions);`,
+      });
+      expect(result.code).toMatchSnapshot();
+    });
+
+    it('should transform process.title', () => {
+      const result = compileModule({
+        code: `console.log(process.title);`,
+      });
+      expect(result.code).toMatchSnapshot();
+    });
+
+    it('should transform process.umask', () => {
+      const result = compileModule({
+        code: `console.log(process.umask);`,
+      });
+      expect(result.code).toMatchSnapshot();
+    });
+
+    it('should transform process.browser', () => {
+      const result = compileModule({
+        code: `console.log(process.browser);`,
+      });
+      expect(result.code).toMatchSnapshot();
+    });
+
+    it('should transform process.cwd', () => {
+      const result = compileModule({
+        code: `console.log(process.cwd());`,
+      });
+      expect(result.code).toMatchSnapshot();
+    });
+
+    it('should transform process.env', () => {
+      const result = compileModule({
+        code: `console.log(process.env);`,
+        bundleProps: {
+          target: 'browser',
+          env: { foo: 'bar' },
+        },
+      });
+      expect(result.code).toMatchSnapshot();
+    });
+
+    it('should transform process.env (not add twice)', () => {
+      const result = compileModule({
+        code: `
+        alert(process.env)
+        console.log(process.env);
+        `,
+        bundleProps: {
+          target: 'browser',
+          env: { foo: 'bar' },
+        },
+      });
+      expect(result.code).toMatchSnapshot();
+    });
+
+    it('should inject process if a variable is not recognized', () => {
+      const result = compileModule({
+        code: `
+        console.log(process.some());
+        `,
+      });
+
+      expect(result.code).toMatchSnapshot();
     });
   });
 });
