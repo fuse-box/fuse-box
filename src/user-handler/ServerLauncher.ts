93455931edb0de08026190a07d5cbc4987ba8eea
Nathan Spaeth <nathan.spaeth@gmail.com>
2019-10-14 12:03:19 -0500

    feat: Add `options` parameter to `ServerLauncher`
    This allows for, e.g.,  passing `process.env` to custom server launches.

--- src/user-handler/ServerLauncher.ts
+++ src/user-handler/ServerLauncher.ts
@@ -1,6 +1,12 @@
-import { spawn } from 'child_process';
+import { spawn, SpawnOptions } from 'child_process';
 import { BundleType, IBundleWriteResponse } from '../bundle/Bundle';
 import { Context } from '../core/Context';
+
+interface handleEntryProps {
+  nodeArgs: Array<string>
+  scriptArgs: Array<string>
+  options?: SpawnOptions
+}
 
 export class ServerLauncher {
   private node;
@@ -9,21 +15,18 @@ export class ServerLauncher {
   public kill() {
     if (this.node) this.node.kill();
   }
-  public handleEntry(props?: { nodeArgs: Array<string>; scriptArgs: Array<string> }) {
+  public handleEntry(props?: handleEntryProps) {
     this.kill();
-    let nodeArgs = [];
-    let scriptArgs = [];
-    if (props) {
-      if (props.nodeArgs) nodeArgs = props.nodeArgs;
-      if (props.scriptArgs) scriptArgs = props.scriptArgs;
-    }
-
+    const nodeArgs = props && props.nodeArgs || [];
+    const scriptArgs = props && props.scriptArgs || [];
+    const options = props && props.options || {};
     const serverEntry = this.response.find(item => item.bundle.props.type == BundleType.SERVER_ENTRY);
     if (!serverEntry) {
       return this.ctx.fatal('Server entry was not found', ['Make sure your dist contains server entry']);
     }
     this.node = spawn('node', [...nodeArgs, serverEntry.stat.absPath, ...scriptArgs], {
       stdio: 'inherit',
+      ...options,
     });
   }
 }
