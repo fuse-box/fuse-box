cfe973d260481a454edaac52036d3464a00c134b
Unknown <nchanged@gmail.com>
2019-10-29 16:09:49 +0100

    chore: playground

---
+++ playground/server/packages/foobar/src/utils.ts
@@ -0,0 +1,3 @@
+export function util() {
+  require('./lib/foobar');
+}
--- playground/server/src/index.ts
+++ playground/server/src/index.ts
@@ -1,2 +1,22 @@
-import * as v8 from 'v8';
-console.log(v8);
+const winston = require('winston');
+
+import 'winston/lib/winston/transports/http';
+// const logger = winston.createLogger({
+//   level: 'info',
+//   format: winston.format.json(),
+//   defaultMeta: { service: 'user-service' },
+//   transports: [
+//     //
+//     // - Write to all logs with level `info` and below to `combined.log`
+//     // - Write all logs error (and below) to `error.log`.
+//     //
+//     new winston.transports.Http({
+//       level: 'warn',
+//       format: winston.format.json(),
+//     }),
+//     new winston.transports.File({ filename: 'error.log', level: 'error' }),
+//     new winston.transports.File({ filename: 'combined.log' }),
+//   ],
+// });
+
+// console.log(logger);
