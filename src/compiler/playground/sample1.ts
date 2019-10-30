07590739c0e054cf46690a8be81b6c4bc972f3d7
Unknown <nchanged@gmail.com>
2019-10-30 16:08:14 +0100

    fix(compiler): declare module fixes + improvements 4.0.0-alpha.234

--- src/compiler/playground/sample1.ts
+++ src/compiler/playground/sample1.ts
@@ -1,17 +1,9 @@
-import { Component, ChangeDetectionStrategy, Renderer2 } from '@angular/core';
-import { AppInitService } from '../shared/fusing/app-init.service';
-import { PlatformService } from '../shared/fusing/platform.service';
-
-@Component({
-  selector: 'pm-app',
-  styleUrls: ['./app.component.css'],
-  templateUrl: './app.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush,
-})
-export class AppComponent {
-  constructor(rdr: Renderer2, ais: AppInitService, ps: PlatformService) {
-    if (ps.isElectron) {
-      ais.init(rdr);
-    }
+declare module 'file-type' {
+  namespace fileType {
+    const minimumBytes: number;
   }
+
+  function fileType(input: Buffer | Uint8Array): { ext: string; mime: string } | null;
+
+  export = fileType;
 }
--- src/compiler/transformers/ts/CommonTSfeaturesTransformer.ts
+++ src/compiler/transformers/ts/CommonTSfeaturesTransformer.ts
@@ -4,41 +4,46 @@ import { IVisit, IVisitorMod } from '../../Visitor/Visitor';
 // to test: function maybeUnwrapEmpty<T>(value: T[]): T[];
 // to test: (oi as any).foo
 export function CommonTSfeaturesTransformer(): ITransformer {
-  return (visit: IVisit): IVisitorMod => {
-    const { node } = visit;
-
-    //console.log(node);
-
-    if (node.declare) {
-      return { removeNode: true, ignoreChildren: true };
-    }
-
-    if (node.type === 'TypeAssertion' || node.type === 'NonNullExpression') {
-      return { replaceWith: node.expression };
-    }
-
-    if (node.type === 'MethodDefinition' && node.value && node.value.type === 'EmptyBodyFunctionExpression') {
-      return { removeNode: true };
-    }
-    switch (node.type) {
-      case 'ParameterProperty':
-        return { replaceWith: node.parameter };
-      case 'AsExpression':
-        return { replaceWith: node.expression };
-      case 'DeclareFunction':
-      case 'TypeAliasDeclaration':
-      case 'AbstractMethodDefinition':
-      case 'InterfaceDeclaration':
-      case 'ClassProperty':
+  return {
+    onTopLevelTraverse: (visit: IVisit): IVisitorMod => {
+      if (visit.node.declare) {
         return { removeNode: true, ignoreChildren: true };
-      case 'ExportNamedDeclaration':
-        const decl = node.declaration;
-        if (decl) {
-          if (decl.declare || decl.type === 'InterfaceDeclaration') {
-            return { removeNode: true, ignoreChildren: true };
+      }
+    },
+    onEachNode: (visit: IVisit): IVisitorMod => {
+      const { node } = visit;
+
+      if (node.declare) {
+        return { removeNode: true, ignoreChildren: true };
+      }
+
+      if (node.type === 'TypeAssertion' || node.type === 'NonNullExpression') {
+        return { replaceWith: node.expression };
+      }
+
+      if (node.type === 'MethodDefinition' && node.value && node.value.type === 'EmptyBodyFunctionExpression') {
+        return { removeNode: true };
+      }
+      switch (node.type) {
+        case 'ParameterProperty':
+          return { replaceWith: node.parameter };
+        case 'AsExpression':
+          return { replaceWith: node.expression };
+        case 'DeclareFunction':
+        case 'TypeAliasDeclaration':
+        case 'AbstractMethodDefinition':
+        case 'InterfaceDeclaration':
+        case 'ClassProperty':
+          return { removeNode: true, ignoreChildren: true };
+        case 'ExportNamedDeclaration':
+          const decl = node.declaration;
+          if (decl) {
+            if (decl.declare || decl.type === 'InterfaceDeclaration') {
+              return { removeNode: true, ignoreChildren: true };
+            }
           }
-        }
-        break;
-    }
+          break;
+      }
+    },
   };
 }
