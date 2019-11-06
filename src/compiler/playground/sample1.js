a202c86e39348b3bbbd755ba392d7e17ba0ec47d
Unknown <nchanged@gmail.com>
2019-11-06 18:41:24 +0100

    chore: playground

--- src/compiler/playground/sample1.js
+++ src/compiler/playground/sample1.js
@@ -1,3 +1,6 @@
-const foo = () => {
-    return () => '';
-};
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var translationKeys;
+(function (translationKeys) {
+    translationKeys["Foo"] = "sadf";
+})(translationKeys = exports.translationKeys || (exports.translationKeys = {}));
