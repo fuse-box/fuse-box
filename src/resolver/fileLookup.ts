1404d84408ccca9027e6eba6c6d599c1f2e8cfb3
Seph <ShadyWillowCreek@gmail.com>
2019-12-11 17:32:12 -0600

    fix(indentation): tabs -> spaces

--- src/resolver/fileLookup.ts
+++ src/resolver/fileLookup.ts
@@ -7,27 +7,27 @@ import { fileExists } from '../utils/utils';
 import { findUp } from '../utils/findUp';
 
 export interface ILookupProps {
-	typescriptFirst?: boolean;
-	javascriptFirst?: boolean;
-	isDev?: boolean;
-	fileDir?: string;
-	filePath?: string;
-	target: string;
+  typescriptFirst?: boolean;
+  javascriptFirst?: boolean;
+  isDev?: boolean;
+  fileDir?: string;
+  filePath?: string;
+  target: string;
 }
 
 export interface TsConfigAtPath {
-	absPath: string;
-	tsConfig: TypescriptConfig;
+  absPath: string;
+  tsConfig: TypescriptConfig;
 }
 
 export interface ILookupResult {
-	isDirectoryIndex?: boolean;
-	fileExists: boolean;
-	absPath: string;
-	extension?: string;
-	monorepoModulesPaths?: string;
-	tsConfigAtPath?: TsConfigAtPath;
-	customIndex?: boolean;
+  isDirectoryIndex?: boolean;
+  fileExists: boolean;
+  absPath: string;
+  extension?: string;
+  monorepoModulesPaths?: string;
+  tsConfigAtPath?: TsConfigAtPath;
+  customIndex?: boolean;
 }
 
 const JS_INDEXES = ['index.js', 'index.jsx'];
@@ -42,146 +42,146 @@ const TS_EXTENSIONS_FIRST = [...TS_EXTENSIONS, ...JS_EXTENSIONS];
 const JS_EXTENSIONS_FIRST = [...JS_EXTENSIONS, ...TS_EXTENSIONS];
 
 function tryIndexes(target: string, indexes: Array<string>) {
-	for (const i in indexes) {
-		const indexFile = indexes[i];
-		const resolved = path.join(target, indexFile);
-		if (fileExists(resolved)) {
-			return resolved;
-		}
-	}
+  for (const i in indexes) {
+    const indexFile = indexes[i];
+    const resolved = path.join(target, indexFile);
+    if (fileExists(resolved)) {
+      return resolved;
+    }
+  }
 }
 
 function tryExtensions(target: string, extensions: Array<string>) {
-	for (const i in extensions) {
-		const resolved = `${target}${extensions[i]}`;
-		if (fileExists(resolved)) {
-			return resolved;
-		}
-	}
+  for (const i in extensions) {
+    const resolved = `${target}${extensions[i]}`;
+    if (fileExists(resolved)) {
+      return resolved;
+    }
+  }
 }
 
 export function fileLookup(props: ILookupProps): ILookupResult {
-	if (!props.fileDir && !props.filePath) {
-		throw new Error('Failed to lookup. Provide either fileDir or filePath');
-	}
-	let resolved = path.join(props.filePath ? path.dirname(props.filePath) : props.fileDir, props.target);
-	const extension = path.extname(resolved);
+  if (!props.fileDir && !props.filePath) {
+    throw new Error('Failed to lookup. Provide either fileDir or filePath');
+  }
+  let resolved = path.join(props.filePath ? path.dirname(props.filePath) : props.fileDir, props.target);
+  const extension = path.extname(resolved);
 
-	if (extension && fileExists(resolved)) {
-		const stat = fs.lstatSync(resolved);
+  if (extension && fileExists(resolved)) {
+    const stat = fs.lstatSync(resolved);
 
-		if (stat.isFile()) {
-			return {
-				extension: path.extname(resolved),
-				absPath: resolved,
-				fileExists: fileExists(resolved),
-			};
-		}
-	}
+    if (stat.isFile()) {
+      return {
+        extension: path.extname(resolved),
+        absPath: resolved,
+        fileExists: fileExists(resolved),
+      };
+    }
+  }
 
-	// try files without extensions first
-	let fileExtensions: Array<string> = TS_EXTENSIONS_FIRST;
-	if (props.javascriptFirst) {
-		fileExtensions = JS_EXTENSIONS_FIRST;
-	}
-	if (props.typescriptFirst) {
-		fileExtensions = TS_EXTENSIONS_FIRST;
-	}
-	const targetFile = tryExtensions(resolved, fileExtensions);
-	if (targetFile) {
-		return {
-			absPath: targetFile,
-			extension: path.extname(targetFile),
-			fileExists: true,
-		};
-	}
+  // try files without extensions first
+  let fileExtensions: Array<string> = TS_EXTENSIONS_FIRST;
+  if (props.javascriptFirst) {
+    fileExtensions = JS_EXTENSIONS_FIRST;
+  }
+  if (props.typescriptFirst) {
+    fileExtensions = TS_EXTENSIONS_FIRST;
+  }
+  const targetFile = tryExtensions(resolved, fileExtensions);
+  if (targetFile) {
+    return {
+      absPath: targetFile,
+      extension: path.extname(targetFile),
+      fileExists: true,
+    };
+  }
 
-	let isDirectory: boolean;
-	// try directory indexes
-	const exists = fileExists(resolved);
-	if (exists) {
-		const stat = fs.lstatSync(resolved);
-		if (stat.isDirectory) {
-			isDirectory = true;
+  let isDirectory: boolean;
+  // try directory indexes
+  const exists = fileExists(resolved);
+  if (exists) {
+    const stat = fs.lstatSync(resolved);
+    if (stat.isDirectory) {
+      isDirectory = true;
 
-			let monorepoModulesPaths;
-			let tsConfigAtPath: TsConfigAtPath;
+      let monorepoModulesPaths;
+      let tsConfigAtPath: TsConfigAtPath;
 
 
-			// only in case of a directory
-			const projectRoot = path.join(resolved, "../../../"); // TODO: get the actual projectRoot
-			const packageJSONPath = findUp(resolved, 'package.json', {
-				boundary: projectRoot,
-				inclusive: false,
-			})
-			if (fileExists(packageJSONPath)) {
-				const useLocalMain = !/node_modules/.test(packageJSONPath);
-				const packageJSON = require(packageJSONPath);
-				const entry = getFolderEntryPointFromPackageJSON({ json: packageJSON, useLocalField: useLocalMain });
+      // only in case of a directory
+      const projectRoot = path.join(resolved, "../../../"); // TODO: get the actual projectRoot
+      const packageJSONPath = findUp(resolved, 'package.json', {
+        boundary: projectRoot,
+        inclusive: false,
+      })
+      if (fileExists(packageJSONPath)) {
+        const useLocalMain = !/node_modules/.test(packageJSONPath);
+        const packageJSON = require(packageJSONPath);
+        const entry = getFolderEntryPointFromPackageJSON({ json: packageJSON, useLocalField: useLocalMain });
 
-				if (useLocalMain && packageJSON['local:main']) {
-					const _monoModules = path.resolve(path.dirname(packageJSONPath), 'node_modules');
-					if (fileExists(_monoModules)) {
-						monorepoModulesPaths = _monoModules;
-					}
+        if (useLocalMain && packageJSON['local:main']) {
+          const _monoModules = path.resolve(path.dirname(packageJSONPath), 'node_modules');
+          if (fileExists(_monoModules)) {
+            monorepoModulesPaths = _monoModules;
+          }
 
-					const _tsConfig = findUp(resolved, 'tsconfig.json', {
-						boundary: projectRoot,
-						inclusive: true,
-					});
-					if (fileExists(_tsConfig)) {
-						const props: any = { tsConfig: _tsConfig };
-						const _tsConfigObject = initTypescriptConfig(props);
-						tsConfigAtPath = { absPath: path.dirname(_tsConfig), tsConfig: _tsConfigObject };
-					}
-				}
+          const _tsConfig = findUp(resolved, 'tsconfig.json', {
+            boundary: projectRoot,
+            inclusive: true,
+          });
+          if (fileExists(_tsConfig)) {
+            const props: any = { tsConfig: _tsConfig };
+            const _tsConfigObject = initTypescriptConfig(props);
+            tsConfigAtPath = { absPath: path.dirname(_tsConfig), tsConfig: _tsConfigObject };
+          }
+        }
 
-				const entryFile = path.join(resolved, entry);
-				return {
-					customIndex: true,
-					monorepoModulesPaths,
-					tsConfigAtPath,
-					isDirectoryIndex: true,
-					absPath: entryFile,
-					extension: path.extname(entryFile),
-					fileExists: fileExists(entryFile),
-				};
-			}
+        const entryFile = path.join(resolved, entry);
+        return {
+          customIndex: true,
+          monorepoModulesPaths,
+          tsConfigAtPath,
+          isDirectoryIndex: true,
+          absPath: entryFile,
+          extension: path.extname(entryFile),
+          fileExists: fileExists(entryFile),
+        };
+      }
 
-			let indexes: Array<string> = TS_INDEXES_FIRST;
-			if (props.javascriptFirst) {
-				indexes = JS_INDEXES_FIRST;
-			}
-			if (props.typescriptFirst) {
-				indexes = TS_INDEXES_FIRST;
-			}
-			const directoryIndex = tryIndexes(resolved, indexes);
-			if (directoryIndex) {
-				return {
-					isDirectoryIndex: true,
-					absPath: directoryIndex,
-					extension: path.extname(directoryIndex),
-					fileExists: true,
-				};
-			}
-		}
-	}
+      let indexes: Array<string> = TS_INDEXES_FIRST;
+      if (props.javascriptFirst) {
+        indexes = JS_INDEXES_FIRST;
+      }
+      if (props.typescriptFirst) {
+        indexes = TS_INDEXES_FIRST;
+      }
+      const directoryIndex = tryIndexes(resolved, indexes);
+      if (directoryIndex) {
+        return {
+          isDirectoryIndex: true,
+          absPath: directoryIndex,
+          extension: path.extname(directoryIndex),
+          fileExists: true,
+        };
+      }
+    }
+  }
 
-	// as a last resort, we should try ".json" which is a very rare case
-	// that's why it has the lowest priority here
-	if (!isDirectory) {
-		const targetFile = tryExtensions(resolved, ['.json']);
-		if (targetFile) {
-			return {
-				customIndex: true, // it still needs to be re-written because FuseBox client API won't find it
-				absPath: targetFile,
-				extension: path.extname(targetFile),
-				fileExists: true,
-			};
-		}
-	}
-	return {
-		fileExists: false,
-		absPath: resolved,
-	};
+  // as a last resort, we should try ".json" which is a very rare case
+  // that's why it has the lowest priority here
+  if (!isDirectory) {
+    const targetFile = tryExtensions(resolved, ['.json']);
+    if (targetFile) {
+      return {
+        customIndex: true, // it still needs to be re-written because FuseBox client API won't find it
+        absPath: targetFile,
+        extension: path.extname(targetFile),
+        fileExists: true,
+      };
+    }
+  }
+  return {
+    fileExists: false,
+    absPath: resolved,
+  };
 }
