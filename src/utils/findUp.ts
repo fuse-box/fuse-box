8ba92fe5fa00b7a42df520185ff19a5271cc0415
Seph <ShadyWillowCreek@gmail.com>
2019-12-11 17:50:47 -0600

    fix(findUp): better while loop syntax

--- src/utils/findUp.ts
+++ src/utils/findUp.ts
@@ -12,12 +12,7 @@ export function findUp(start: string, target: string, boundaryArg?: {
 	let currentDir = dirname(start);
 	let lastTry = false;
 	let backSteps = 0;
-	while (++backSteps) {
-		if (backSteps >= FIND_UP_GLOBAL_CONFIG.maxStepsBack) {
-			console.error("Too many back steps");
-			return null;
-		}
-
+	while (backSteps++ <= FIND_UP_GLOBAL_CONFIG.maxStepsBack) {
 		if (boundaryArg && boundaryArg.boundary.includes(currentDir)) {
 			if (boundaryArg.inclusive && lastTry === false) {
 				lastTry = true;
@@ -33,4 +28,7 @@ export function findUp(start: string, target: string, boundaryArg?: {
 
 		currentDir = join(currentDir, "../");
 	}
+
+	console.error(`Too many back steps, starting from "${start}"`);
+	return null;
 }
