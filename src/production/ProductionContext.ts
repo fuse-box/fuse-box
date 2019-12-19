65ff89d32781d53bd5f93893c1fb69e04c6e535a
nchanged <nchanged@gmail.com>
2019-12-19 13:09:47 +0100

    feat: wip compiler

--- src/production/ProductionContext.ts
+++ src/production/ProductionContext.ts
@@ -1,6 +1,10 @@
 import { Context } from '../core/Context';
 import { Package } from '../core/Package';
 
-export class ProductionContext {
-  constructor(public ctx: Context, public packages: Array<Package>) {}
+export function ProductionContext(ctx: Context, packages: Array<Package>) {
+  return {
+    packages,
+    ctx,
+  };
 }
+export type IProductionContext = ReturnType<typeof ProductionContext>;
