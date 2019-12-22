c98cbc497eb72415d420188b3b11c367aeb568cd
Ferry Kobus <ferry@webmonkeys.nl>
2019-12-22 23:43:54 +0100

    wip: warmup imports

--- playground/newProdTest/src/index.ts
+++ playground/newProdTest/src/index.ts
@@ -2,6 +2,18 @@
 
 import './index.scss';
 import { util } from './utils';
+
+import styled from '@emotion/styled';
+import * as React from 'react';
+import React from 'react';
+import React, { Fragment } from 'react';
+import './foo';
+import { bar, foo } from './foo';
+import { joo, test as something } from './superAwesome';
+const bar = require('foo');
+const baz = 'foo';
+import _ = require('foo');
+
 console.log('hello world');
 
 async function aaaa() {
--- src/production/transformers/Phase_1_ImportLink.ts
+++ src/production/transformers/Phase_1_ImportLink.ts
@@ -1,29 +1,127 @@
-import { ITransformer } from '../../compiler/interfaces/ITransformer';
+import { ProductionContext } from '../../____production/ProductionContext';
 import { IVisit, IVisitorMod } from '../../compiler/Visitor/Visitor';
+import { ASTNode } from '../../compiler/interfaces/AST';
+import { ITransformer } from '../../compiler/interfaces/ITransformer';
+import { Module } from '../../core/Module';
 
-export enum TreeReferenceType {
+export enum ModuleRefType {
   SIDE_EFFECT_IMPORT,
   IMPORT_SPECIFIERS,
   DYNAMIC_IMPORT,
   EXPORT,
+};
+
+function ImportSpecifier(visit: IVisit, specifier: ASTNode): ASTNode {
+  let local: string;
+  let name: string;
+
+  if (specifier.type === 'ImportNamespaceSpecifier') {
+    // import * as React from 'react'
+    local = specifier.local.name;
+    name = 'default';
+  } else if (specifier.type === 'ImportDefaultSpecifier') {
+    // import styled from '@emotion/styled'
+    local = specifier.local.name;
+    name = 'default';
+  } else if (specifier.type === 'ImportSpecifier') {
+    // import { something } from 'some-module'
+    local = specifier.local.name;
+    name = specifier.imported.name;
+  }
+
+  return {
+    local,
+    name,
+    remove: function () {
+      if (visit.node.specifiers instanceof Array) {
+        const index = visit.node.specifiers.indexOf(specifier);
+        if (index > -1) {
+          visit.node.specifiers.splice(index, 1);
+        }
+      }
+    }
+  };
+};
+
+function ImportReference(props: {
+  module: Module,
+  productionContext: ProductionContext,
+  visit: IVisit
+}) {
+  let type: ModuleRefType;
+  let specifiers: Array<ASTNode> = [];
+  let source: ASTNode;
+
+  const node = props.visit.node;
+
+  if (node.type === 'ImportDeclaration') {
+    // set the source
+    source = node.source;
+    if (node.specifiers.length === 0) {
+      // import './foo';
+      type = ModuleRefType.SIDE_EFFECT_IMPORT;
+    } else {
+      // import xx from 'xx';
+      // import { yy, bb as cc } from 'yy';
+      // import zz as aa from 'zz'
+      for (const specifier of node.specifiers) {
+        specifiers.push(ImportSpecifier(props.visit, specifier));
+      }
+      type = ModuleRefType.IMPORT_SPECIFIERS;
+    }
+
+  } else if (
+    node.type === 'CallExpression' &&
+    node.callee.name === 'require'
+  ) {
+    // const bar = require('foo');
+    type = {
+      type: 'Literal',
+      value: ModuleRefType.SIDE_EFFECT_IMPORT
+    };
+    source = node.arguments[0].value;
+
+  } else if (node.type === 'ImportEqualsDeclaration') {
+    // import _ = require('foo');
+    type = ModuleRefType.SIDE_EFFECT_IMPORT;
+    source = {
+      type: 'Literal',
+      value: node.moduleReference.expression.value
+    };
+  }
+
+  if (ModuleRefType[type]) {
+    return {
+      remove: function () {
+        if (props.visit.property && props.visit.parent) {
+          if (props.visit.parent[props.visit.property] instanceof Array) {
+            const index = props.visit.parent[props.visit.property].indexOf(node);
+            if (index > -1) {
+              props.visit.parent[props.visit.property].splice(index, 1);
+            }
+          }
+        }
+      },
+      source,
+      specifiers,
+      type
+    };
+  }
+  return;
 }
+
 export function Phase_1_ImportLink(): ITransformer {
   return {
     productionWarmupPhase: ({ ctx, module, productionContext }) => {
       const tree = module.moduleTree;
+      const refs = module.moduleSourceRefs;
       return {
-        onTopLevelTraverse: (visit: IVisit) => {
-          const { node } = visit;
-          if (node.type === 'ImportDeclaration') {
-            if (node.specifiers.length === 0) {
-              tree['stuff'] = true;
-              // find module in refs and also add TreeReferenceType
-              //tree.externalReferences.add( )
-            }
-          }
+        onEachNode: (visit: IVisit): IVisitorMod => {
           return;
         },
-        onEachNode: (visit: IVisit): IVisitorMod => {
+        onTopLevelTraverse: (visit: IVisit) => {
+          const importReference = ImportReference({ module, productionContext, visit });
+          // console.log(importReference);
           return;
         },
       };
