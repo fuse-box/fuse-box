2b7845ce81425993a47bf2357ebe1ec6f9b82207
Ferry Kobus <ferry@famkob.us>
2020-01-06 19:46:09 +0100

    Feat: production warmup imports (#1741)
    * feat: production build phase1 import warmup

--- playground/newProdTest/src/index.ts
+++ playground/newProdTest/src/index.ts
@@ -2,6 +2,18 @@
 
 import './index.scss';
 import { util } from './utils';
+
+import styled from '@emotion/styled';
+import * as React from 'react';
+import React from 'react';
+import React, { Fragment } from 'react';
+import './foo';
+import { bar, foo } from './foo';
+import { joo, test as something } from './superAwesome';
+const bar = require('./foo');
+const baz = 'foo';
+import _ = require('./foo');
+
 console.log('hello world');
 
 async function aaaa() {
@@ -9,3 +21,10 @@ async function aaaa() {
   console.log(res);
 }
 util();
+
+async function aaaa() {
+  const res = await import('./split');
+}
+const dynImp1 = () => import('./dyn/dyn1');
+const dynImp2 = import('./dyn/dyn2');
+const dynImp3Ignore = import('./dyn/dyn3');
--- src/production/transformers/Phase_1_ImportLink.ts
+++ src/production/transformers/Phase_1_ImportLink.ts
@@ -1,32 +1,68 @@
-import { ITransformer } from '../../compiler/interfaces/ITransformer';
 import { IVisit, IVisitorMod } from '../../compiler/Visitor/Visitor';
+import { ASTNode } from '../../compiler/interfaces/AST';
+import { ITransformer } from '../../compiler/interfaces/ITransformer';
+import { Module } from '../../core/Module';
 
-export enum TreeReferenceType {
-  SIDE_EFFECT_IMPORT,
-  IMPORT_SPECIFIERS,
-  DYNAMIC_IMPORT,
-  EXPORT,
-}
+const NODES_OF_INTEREST = {
+  ExportAllDeclaration: 1,
+  ExportNamedDeclaration: 1,
+  ImportDeclaration: 1,
+  ImportEqualsDeclaration: 1
+};
+
 export function Phase_1_ImportLink(): ITransformer {
   return {
-    productionWarmupPhase: ({ ctx, module, productionContext }) => {
+    productionWarmupPhase: ({ module, productionContext }) => {
       const tree = module.moduleTree;
+      const refs = module.moduleSourceRefs;
+
+      function isEligibleRequire(node): boolean {
+        return (
+          node.type === 'CallExpression' &&
+          node.callee.name === 'require' &&
+          node.arguments.length === 1 &&
+          node.arguments[0].type === 'Literal' &&
+          refs[node.arguments[0].value]
+        );
+      }
+
+      function isEligibleImportOrExport(node): boolean {
+        return (
+          NODES_OF_INTEREST[node.type] &&
+          (
+            (node.source && refs[node.source.value]) ||
+            (node.moduleReference && refs[node.moduleReference.expression.value])
+          )
+        );
+      }
+
+      function isEligibleDynamicImport(node): boolean {
+        return (
+          node.type === 'ImportExpression' &&
+          node.source &&
+          refs[node.source.value]
+        )
+      }
+
       return {
+        onEachNode: (visit: IVisit): IVisitorMod => {
+          if (visit.parent && visit.parent.type === 'Program') {
+            return;
+          }
+
+          const { node } = visit;
+          if (isEligibleDynamicImport(node) || isEligibleRequire(node)) {
+            tree.importReferences.register({ module, productionContext, visit });
+          }
+        },
+
         onTopLevelTraverse: (visit: IVisit) => {
           const { node } = visit;
-          if (node.type === 'ImportDeclaration') {
-            if (node.specifiers.length === 0) {
-              tree['stuff'] = true;
-              // find module in refs and also add TreeReferenceType
-              //tree.externalReferences.add( )
-            }
+          if (isEligibleImportOrExport(node) || isEligibleRequire(node)) {
+            tree.importReferences.register({ module, productionContext, visit });
           }
-          return;
-        },
-        onEachNode: (visit: IVisit): IVisitorMod => {
-          return;
-        },
+        }
       };
-    },
+    }
   };
 }
