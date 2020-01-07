cbb1f4c913151a014816ec006f0bbc35e41c5182
Ferry Kobus <ferry.kobus@massive.se>
2020-01-07 16:17:10 +0100

    chore: wip - prod code splitting

--- playground/newProdCodeSplitting/src/dynamic/dynamic.js
+++ playground/newProdCodeSplitting/src/dynamic/dynamic.js
@@ -1,7 +1,9 @@
 import { counter } from '../utils/counter';
+import { path } from '../utils/path';
 
 export default () => {
   console.log('hi, my name is dynamicFunc');
 
   counter();
+  path();
 }
--- playground/newProdCodeSplitting/src/index.js
+++ playground/newProdCodeSplitting/src/index.js
@@ -2,8 +2,12 @@ import { alsoNotSoStaticFunc } from './static/alsoNotSoStaticFunc';
 import { notSoStaticFunc } from './static/notSoStatic';
 import { staticFunc } from './static/static';
 
+import { path } from './utils/path';
+
 console.log('hi, my name is index.js');
 
 staticFunc();
 notSoStaticFunc();
 alsoNotSoStaticFunc();
+
+path();
