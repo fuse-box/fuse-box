d636a44de44932327676a4ce392ddc9707db2b4b
Unknown <nchanged@gmail.com>
2020-01-14 08:15:32 +0100

    chore: react playground

--- playground/react/src/index.tsx
+++ playground/react/src/index.tsx
@@ -1,22 +1,5 @@
-import * as React from 'react';
-import { Component } from 'react';
-import * as ReactDOM from 'react-dom';
-import './index.scss';
-import { Other } from './Other';
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import { Application } from "./components/application/Application";
 
-class App extends Component {
-  private click() {
-    const data = 1;
-    console.log('hello world', data);
-  }
-  render() {
-    return (
-      <div onClick={() => this.click()}>
-        22222222
-        <Other></Other>
-      </div>
-    );
-  }
-}
-
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Application />, document.getElementById("root"));
