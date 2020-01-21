f5c3e89f8992369e986c9a00934d885d0039b8a6
Ferry Kobus <ferry@famkob.us>
2020-01-21 14:46:50 +0100

    Delete constants.ts

--- src/BundleRuntime/constants.ts
+++
@@ -1,7 +0,0 @@
-export function sourceMapsURL(file: string) {
-  return `//# sourceMappingURL=${file}`;
-}
-
-export function sourceMapsCSSURL(file: string) {
-  return `/*# sourceMappingURL=${file} */`;
-}
