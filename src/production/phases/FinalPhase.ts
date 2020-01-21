283d25b5651681535a51467b6d58fea3d5df9ace
FKobus <ferry@famkob.us>
2020-01-21 11:48:28 +0100

    chore: set up final phase

---
+++ src/production/phases/FinalPhase.ts
@@ -0,0 +1,19 @@
+import { IProductionContext } from '../ProductionContext';
+
+/**
+ * @todo:
+ * - Create a new phase
+ * - Loop modules and call transpile()
+ * - fix require statements
+ * - call generate()
+ * - pipe that shit BundleRouter
+ * - fix bundle router to understand splitEntry
+ */
+
+export function FinalPhase(productionContext: IProductionContext) {
+  for (const module of productionContext.modules) {
+    if (module.isExecutable) {
+      module.transpile();
+    }
+  }
+}
