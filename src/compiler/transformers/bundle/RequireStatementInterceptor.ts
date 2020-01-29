c58111bbbaf2d23b4a915a330910ec129dcb4f8e
FKobus <ferry@famkob.us>
2020-01-29 12:21:23 +0100

    chore: fixing bugs

--- src/compiler/transformers/bundle/RequireStatementInterceptor.ts
+++ src/compiler/transformers/bundle/RequireStatementInterceptor.ts
@@ -1,6 +1,6 @@
-import { ImportType } from '../../interfaces/ImportType';
-import { ITransformer } from '../../interfaces/ITransformer';
 import { IVisit, IVisitorMod } from '../../Visitor/Visitor';
+import { ITransformer } from '../../interfaces/ITransformer';
+import { ImportType } from '../../interfaces/ImportType';
 
 export function RequireStatementInterceptor(): ITransformer {
   return {
@@ -12,10 +12,13 @@ export function RequireStatementInterceptor(): ITransformer {
             return;
           }
           if (node.type === 'CallExpression' && node.callee.name === 'require' && !node['emitted']) {
-            const locals = visit.scope && visit.scope.locals ? visit.scope.locals : {};
-            if (!locals[node.name]) {
-              props.onRequireCallExpression(ImportType.REQUIRE, node);
-            }
+            // broken code for reference (node.name is always undefined)
+            // const locals = (visit.scope && visit.scope.locals) || {};
+            // if (!locals[node.name]) {
+            //   props.onRequireCallExpression(ImportType.REQUIRE, node);
+            // }
+
+            props.onRequireCallExpression(ImportType.REQUIRE, node);
           }
           return;
         },
