967d5276c26892f6d87acdc48b1db9952ebd257d
Unknown <nchanged@gmail.com>
2020-02-06 10:44:52 +0100

    feat: New HMR with plugins

--- src/interceptor/events.ts
+++ src/interceptor/events.ts
@@ -1,10 +1,13 @@
-import { IBundleWriteResponse } from '../bundle/bundle';
+import { Bundle, IBundleWriteResponse } from '../bundle/bundle';
 import { IRunResponse } from '../core/IRunResponse';
 import { Context } from '../core/context';
 import { IBundleContext } from '../moduleResolver/bundleContext';
 import { IModule } from '../moduleResolver/module';
 import { ReactionStack } from '../watcher/watcher';
 
+export interface IEventBeforeBundleWrite {
+  bundle: Bundle;
+}
 export interface InterceptorEvents {
   bundle_resolve_module: { module: IModule };
   init: { ctx: Context };
@@ -18,11 +21,12 @@ export interface InterceptorEvents {
     scriptTags: Array<string>;
   };
 
-  watcher_reaction: { reactionStack: ReactionStack };
-
+  before_bundle_write: IEventBeforeBundleWrite;
   complete: IRunResponse;
   rebundle_complete: IRunResponse;
+  entry_resolve: { module: IModule };
   rebundle: {
     bundles: Array<IBundleWriteResponse>;
   };
+  watcher_reaction: { reactionStack: ReactionStack };
 }
