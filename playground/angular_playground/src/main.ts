989d77df87a79a31d98adb738b5a7cb21131da62
Unknown <nchanged@gmail.com>
2020-02-07 18:12:54 +0100

    chore: playground

--- playground/angular_playground/src/main.ts
+++ playground/angular_playground/src/main.ts
@@ -3,6 +3,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import './main.scss';
 
 if (environment.production) {
   enableProdMode();
