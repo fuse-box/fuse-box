b5061601ce973280168eaa7414c5e9105fe8e7a6
Ferry Kobus <ferry@webmonkeys.nl>
2020-02-13 09:24:19 +0100

    chore: starting building the server

--- src/devServer/server.ts
+++ src/devServer/server.ts
@@ -1,9 +1,17 @@
+import { IBundleWriteResponse } from '../bundle/bundle';
+import { ITarget } from '../config/ITarget';
+import { Context } from '../core/context';
+
 export interface IServer {
+  target: ITarget;
   onComplete: (props: any) => void;
 }
 
-export const createServer = (): IServer => {
+export const createServer = (ctx: Context, bundles: Array<IBundleWriteResponse>): IServer => {
+  const target = ctx.config.target;
+
   return {
+    target,
     onComplete: ({ electron, runResponse, server }): void => {
       console.log(server, electron, runResponse);
     },
