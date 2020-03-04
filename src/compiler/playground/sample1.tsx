6c42ae98ecf94cc41da63917ac69752c0b6acd68
Unknown <nchanged@gmail.com>
2020-03-04 21:06:24 +0100

    chore: cleanup

--- src/compiler/playground/sample1.tsx
+++ src/compiler/playground/sample1.tsx
@@ -1,3 +1,10 @@
-export namespace Hey {
-  //export function hello() {}
+class Bar {
+  // this `foo` decorator not called because `methodNoParam` has no parameter
+  // try to add a parameter -- then it will get called
+  @foo()
+  methodNoParam() {
+    console.log('methodNoParam called');
+
+    return 'methodNoParam';
+  }
 }
