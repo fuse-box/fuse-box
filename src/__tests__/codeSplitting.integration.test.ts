e45caf207dc277a81afedc7e10929a64e1f557a2
Unknown <nchanged@gmail.com>
2020-03-06 11:42:24 +0100

    fix: css modules for production

--- src/__tests__/codeSplitting.integration.test.ts
+++ src/__tests__/codeSplitting.integration.test.ts
@@ -1,4 +1,5 @@
 import { EnvironmentType } from '../config/EnvironmentType';
+import { pluginSass } from '../plugins/core/plugin_sass';
 import { runBrowserTest, runServerTest } from '../testUtils/integrationHelper';
 describe('Code splitting integration test', () => {
   describe('JS + CSS code splitting', () => {
@@ -29,6 +30,35 @@ describe('Code splitting integration test', () => {
         expect(scope.$loadedCSSFiles).toHaveLength(1);
         expect(result.foo).toEqual('foo_module');
       });
+
+      it('should load a file + css module', async () => {
+        const env = await runBrowserTest({
+          env: EnvironmentType.PRODUCTION,
+          config: {
+            plugins: [pluginSass('foo.scss', { asModule: {} })],
+          },
+          files: {
+            'index.ts': `
+            export async function test(){
+              return import("./foo")
+            }
+          `,
+            'foo.ts': `
+              import * as foo from "./foo.scss";
+              export { foo };
+            `,
+            'foo.scss': `.item { color: red}`,
+          },
+        });
+
+        const scope = await env.response.eval();
+        const entry = scope.entry();
+
+        expect(scope.$loadedCSSFiles).toHaveLength(0);
+        const result = await entry.test();
+        expect(scope.$loadedCSSFiles).toHaveLength(1);
+        expect(result.foo.item).toBeTruthy();
+      });
     });
 
     describe('Server', () => {
@@ -54,6 +84,34 @@ describe('Code splitting integration test', () => {
         const entry = scope.entry();
         const result = await entry.test();
         expect(result.foo).toEqual('foo_module');
+      });
+
+      it('should load a file + css module', async () => {
+        const env = await runServerTest({
+          env: EnvironmentType.PRODUCTION,
+          config: {
+            plugins: [pluginSass('foo.scss', { asModule: {} })],
+          },
+          files: {
+            'index.ts': `
+            export async function test(){
+              return import("./foo")
+            }
+          `,
+            'foo.ts': `
+              import * as foo from "./foo.scss";
+              export { foo };
+            `,
+            'foo.scss': `.item { color: red}`,
+          },
+        });
+
+        const scope = await env.response.eval();
+        const entry = scope.entry();
+
+        const result = await entry.test();
+
+        expect(result.foo.item).toBeTruthy();
       });
     });
   });
