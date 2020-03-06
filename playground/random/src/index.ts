9bebd9a093bf6e0d500f2f87be8e625ac1a19a2f
Unknown <nchanged@gmail.com>
2020-03-06 11:42:47 +0100

    chore: playground

--- playground/random/src/index.ts
+++ playground/random/src/index.ts
@@ -1,33 +1,8 @@
-import 'foo';
-// import * as lodash from 'lodash-es';
-// console.log(lodash);
+import io from 'socket.io-client';
+//You are supposed to use io like this
 
-// import * as one from './bench/one/Three';
-// import * as three from './bench/three/Three';
-// import * as two from './bench/two/Three';
+const socket = io(); //👈 this particular line of code wants to know about `punycode`
+//You can then emit stuff to the server
+socket.emit('hey-server');
 
-// console.log(one, two, three);
-// import arrayIncludes from 'array-includes';
-// if (!Array.includes) arrayIncludes.shim();
-
-// import io from 'socket.io-client';
-// //You are supposed to use io like this
-
-// const socket = io(); //👈 this particular line of code wants to know about `punycode`
-// //You can then emit stuff to the server
-// socket.emit('hey-server');
-// console.log(socket);
-// import foo from 'proj4/lib/constants/Ellipsoid.js';
-
-// import * as PIXI from 'pixi.js';
-
-// const app = new PIXI.Application({ antialias: true });
-// document.body.appendChild(app.view);
-
-// const sprite = new PIXI.Sprite(PIXI.Texture.WHITE);
-// sprite.tint = 0xff0000;
-// sprite.position.set(0, 0);
-// sprite.width = 50;
-// sprite.height = 50;
-// console.log(sprite);
-// app.stage.addChild(sprite);
+console.log('jere...');
--- playground/sass/src/index.tsx
+++ playground/sass/src/index.tsx
@@ -1,21 +1,22 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import { Component } from 'react';
+// import { Component } from 'react';
+// import * as React from 'react';
+// import * as ReactDOM from 'react-dom';
 import './index.scss';
+import * as mod from './mod.scss';
+console.log(mod);
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <h1>Hello world</h1>
+//         <p>
+//           To get started, edit
+//           <code>src/App.js</code>
+//           and save to reload.
+//         </p>
+//       </div>
+//     );
+//   }
+// }
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>Hello world</h1>
-        <p>
-          To get started, edit
-          <code>src/App.js</code>
-          and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
-
-ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<App />, document.getElementById('root'));
