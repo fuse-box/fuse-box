dc04f50171a298279ee8c73480f738a5780ab30a
Vegar Ringdal <vegar.ringdal@gmail.com>
2020-03-21 00:33:02 +0100

    fix: module.isExecutable

--- src/plugins/core/plugin_minify_html_literals.ts
+++ src/plugins/core/plugin_minify_html_literals.ts
@@ -15,37 +15,37 @@ export function pluginMinifyHtmlLiterals(...options: IPluginMinifyHtmlLiterals[]
   return (ctx: Context) => {
     ctx.ict.on('bundle_resolve_module', props => {
       const { module } = props;
-
-      // verify if module is installed
-      if (!isNodeModuleInstalled('minify-html-literals')) {
-        ctx.fatal(`Fatal error when capturing ${module.absPath}`, [
-          'Module "minify-html-literals" is required, Please install it using the following command',
-          'npm install minify-html-literals --save-dev',
-        ]);
-        return;
-      }
-      // check if we have any html`` or css`` template tags
-      if (/\html`|css`/.test(module.contents)) {
-        ctx.log.info('pluginMinifyHtmlLiterals', 'minifying $file', {
-          file: module.absPath,
-        });
-        // https://github.com/asyncLiz/minify-html-literals
-        let newcontent;
-        try {
-          const { minifyHTMLLiterals } = require('minify-html-literals');
-          newcontent = minifyHTMLLiterals(module.contents, ...useroptions);
-        } catch (e) {
-          console.log(e);
+      if (module.isExecutable) {
+        // verify if module is installed
+        if (!isNodeModuleInstalled('minify-html-literals')) {
+          ctx.fatal(`Fatal error when capturing ${module.absPath}`, [
+            'Module "minify-html-literals" is required, Please install it using the following command',
+            'npm install minify-html-literals --save-dev',
+          ]);
+          return;
         }
-        if (newcontent && newcontent.code) {
-          module.contents = newcontent.code;
-        } else {
-          ctx.log.error('pluginMinifyHtmlLiterals - failed $file', {
+        // check if we have any html`` or css`` template tags
+        if (/\html`|css`/.test(module.contents)) {
+          ctx.log.info('pluginMinifyHtmlLiterals', 'minifying $file', {
             file: module.absPath,
           });
+          // https://github.com/asyncLiz/minify-html-literals
+          let newcontent;
+          try {
+            const { minifyHTMLLiterals } = require('minify-html-literals');
+            newcontent = minifyHTMLLiterals(module.contents, ...useroptions);
+          } catch (e) {
+            console.log(e);
+          }
+          if (newcontent && newcontent.code) {
+            module.contents = newcontent.code;
+          } else {
+            ctx.log.error('pluginMinifyHtmlLiterals - failed $file', {
+              file: module.absPath,
+            });
+          }
         }
       }
-
       return props;
     });
   };
