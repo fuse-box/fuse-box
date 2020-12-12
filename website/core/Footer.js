81e89509d9dd2387d4aa691ba215ce6aa1158557
Vegar Ringdal <vegar.ringdal@gmail.com>
2020-12-12 20:31:47 +0100

    chore: update footer links to docs and replace gitter with slack

--- website/core/Footer.js
+++ website/core/Footer.js
@@ -1,9 +1,9 @@
-const React = require("react");
+const React = require('react');
 
 class Footer extends React.Component {
   docUrl(doc) {
     const baseUrl = this.props.config.baseUrl;
-    return baseUrl + "docs/" + doc;
+    return baseUrl + 'docs/' + doc;
   }
 
   pageUrl(doc) {
@@ -27,28 +27,12 @@ class Footer extends React.Component {
           </a>
           <div>
             <h5>Docs</h5>
-            <a href={this.docUrl("getting-started/installation")}>
-              Getting Started
-            </a>
-            <a href={this.docUrl("development/configuration")}>Development</a>
-            <a href={this.docUrl("production-builds/quantum")}>
-              Production Builds
-            </a>
-            <a href={this.docUrl("task-runner/getting-started-with-sparky")}>
-              Task Runner
-            </a>
-            <a href={this.docUrl("test-runner/test-runner")}>Test Runner</a>
-            <a href={this.docUrl("guides/working-with-targets")}>Guides</a>
-            <a href={this.docUrl("plugins/babel-plugin")}>Plugins</a>
+            <a href={this.docUrl('getting-started/installation')}>Getting Started</a>
           </div>
           <div>
             <h5>Community</h5>
-            <a href="https://gitter.im/fusebox-bundler/Lobby">Gitter</a>
-            <a
-              href="https://twitter.com/fuseboxjs"
-              target="_blank"
-              rel="noreferrer noopener"
-            >
+            <a href="https://slack.fuse-box.org/">Slack</a>
+            <a href="https://twitter.com/fuseboxjs" target="_blank" rel="noreferrer noopener">
               Twitter
             </a>
           </div>
